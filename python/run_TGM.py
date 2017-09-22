1a15362768176352f278a07d376dc2a04ad2f9e9
nrafidi <nicole.rafidi@gmail.com>
2017-09-22 11:28:20 -0400

    fixed validity check

--- python/run_TGM.py
+++ python/run_TGM.py
@@ -205,7 +205,7 @@ if __name__ == '__main__':
     # Check that parameter setting is valid
     is_valid = args.overlap <= args.win_len
     is_valid = is_valid and (args.reps_to_use <= load_data.NUM_REPS[args.experiment])
-    is_valid = is_valid and ((args.reps_to_use % args.num_instances) == 0)
+    if args.num_instances != 2:
     if is_valid:
         run_tgm_exp(experiment=args.experiment,
                     subject=args.subject,
