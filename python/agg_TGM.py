b5db46d04db026da04da4f474e05688224f9030b
Nicole <nicole.rafidi@gmail.com>
2017-09-25 19:15:43 -0400

    testing agg code

--- python/agg_TGM.py
+++ python/agg_TGM.py
@@ -60,67 +60,87 @@ def extract_param(param_name, fname):
         raise ValueError('Invalid fname')
 
     match_str = '_{}(\d+)_'.format(param_name)
-    print(match_str)
     m = re.match('.*_' + param_name + '(\d+)_.*', fname)
-    print(m)
     if m:
         return m.group(1)
     else:
         raise ValueError('Invalid fname')
 
+def get_param_str(param_name, param_val):
+    if param_name == 'alg':
+        return param_val
+    if param_name == 'F':
+        return '_{}F_'.format(param_val)
+    if isinstance(PARAM_TYPES[param_name], list):
+        return param_name + param_val
 
-def tgm_from_preds(preds, l_index, cv_membership, accuracy):
-    return 'meow'
+    return '_{}{}_'.format(param_name, param_val)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--experiment', default='krns2')
-    parser.add_argument('--mode', default='pred')
-    parser.add_argument('--accuracy', default='abs')
-    args = parser.parse_args()
+def tgm_from_preds(preds, l_index, cv_membership, accuracy='abs'):
+    print(preds.shape)
+    print(l_index.shape)
+    print(cv_membership.shape)
 
-    exp = args.experiment
 
-    subjects = load_data.VALID_SUBS[exp]
+def agg_results(exp, mode, accuracy, sub, param_specs=None):
+    sub_results = {}
+    sub_params = {}
+    sub_time = {}
+    result_dir = run_TGM.SAVE_DIR.format(exp=exp, sub=sub)
+    result_files = os.listdir(result_dir)
+    for f in result_files:
+        valid_file = mode in f
+        if param_specs is not None:
+            for param_name, param_val in param_specs.iteritems():
+                valid_file = valid_file and (get_param_str(param_name, param_val) in f)
+        if valid_file:
+            word = which_word(f)
+            sen_type = which_sen_type(f)
 
-    for sub in subjects:
-        sub_results = {}
-        sub_params = {}
-        sub_time = {}
-        result_dir = run_TGM.SAVE_DIR.format(exp=exp, sub=sub)
-        result_files = os.listdir(result_dir)
-        for i_f in range(2):
-            f = result_files[i_f]
-            print(f)
-            if args.mode in f:
-                word = which_word(f)
-                sen_type = which_sen_type(f)
-
-                if sen_type not in sub_results:
-                    sub_results[sen_type] = {}
-                    sub_params[sen_type] = {}
-                    sub_time[sen_type] = {}
-                if word not in sub_results[sen_type]:
-                    sub_results[sen_type][word] = []
-                    sub_params[sen_type][word] = {}
-                    sub_time[sen_type][word] = {}
-                    for param in PARAMS_TO_AGG:
-                        sub_params[sen_type][word][param] = []
-                    sub_time[sen_type][word]['time'] = []
-                    sub_time[sen_type][word]['win_starts'] = []
-
+            if sen_type not in sub_results:
+                sub_results[sen_type] = {}
+                sub_params[sen_type] = {}
+                sub_time[sen_type] = {}
+            if word not in sub_results[sen_type]:
+                sub_results[sen_type][word] = []
+                sub_params[sen_type][word] = {}
+                sub_time[sen_type][word] = {}
                 for param in PARAMS_TO_AGG:
+                    sub_params[sen_type][word][param] = []
+                sub_time[sen_type][word]['time'] = []
+                sub_time[sen_type][word]['win_starts'] = []
+
+            for param in PARAMS_TO_AGG:
+                if param not in param_specs:
                     param_val = extract_param(param, f)
                     sub_params[sen_type][word][param].append(param_val)
 
-                print(sub_params)
+            print(sub_params)
 
-                # result = np.load(f)
-                # tgm = tgm_from_preds(result['preds'], result['l_index'], result['cv_membership'], args.accuracy)
-                # sub_results[sen_type][word].append(tgm)
-                #
-                # sub_time[sen_type][word]['time'] = result['time']
-                # sub_time[sen_type][word]['win_starts'] = result['win_starts']
+            result = np.load(f)
+            tgm_from_preds(result['preds'], result['l_index'], result['cv_membership'], accuracy)
+            # sub_results[sen_type][word].append(tgm)
 
+            sub_time[sen_type][word]['time'] = result['time']
+            sub_time[sen_type][word]['win_starts'] = result['win_starts']
+
+
+if __name__ == '__main__':
+    exp = 'krns2'
+    mode = 'pred'
+    accuracy = 'abs'
+    sub = 'B'
+    param_specs = {'w': 100,
+                   'o': 12,
+                   'pd': 'F',
+                   'pr': 'F',
+                   'F': 2,
+                   'alg': 'LR',
+                   'z': 'F',
+                   'avg': 'F',
+                   'ni': 2,
+                   'nr': 10,
+                   'rs': 1}
+    agg_results(exp, mode, accuracy, sub, param_specs=param_specs)
 
