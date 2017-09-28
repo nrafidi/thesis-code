b72d47db7016dee921998a524b4c82bfd1a2605c
nrafidi <nicole.rafidi@gmail.com>
2017-09-28 16:03:04 -0400

    plot similarity and accuracy side by side

--- python/plot_krns2.py
+++ python/plot_krns2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import load_data
 import agg_TGM
+import coef_sim
 
 
 if __name__ == '__main__':
@@ -12,20 +13,21 @@ if __name__ == '__main__':
     word = 'firstNoun'
     sen_type = 'active'
     accuracy = 'abs'
+    param_specs = {'o': 12,
+                   'pd': 'F',
+                   'pr': 'F',
+                   'alg': 'LR',
+                   'F': 2,
+                   'z': 'F',
+                   'avg': 'F',
+                   'ni': 2,
+                   'nr': 10,
+                   'rs': 1}
     sub_results = {}
     sub_params = {}
     sub_time = {}
     for sub in ['B']:  # load_data.VALID_SUBS[exp]:
-        param_specs = {'o': 12,
-                       'pd': 'F',
-                       'pr': 'F',
-                       'alg': 'LR',
-                       'F': 2,
-                       'z': 'F',
-                       'avg': 'F',
-                       'ni': 2,
-                       'nr': 10,
-                       'rs': 1}
+
         sub_results[sub], sub_params[sub], sub_time[sub] = agg_TGM.agg_results(exp,
                                                                                mode,
                                                                                word,
@@ -34,18 +36,32 @@ if __name__ == '__main__':
                                                                                sub,
                                                                                param_specs=param_specs)
     diag, param_val, time, _ = agg_TGM.get_diag_by_param(sub_results, sub_params, sub_time, 'w', {})
-
     diag = np.mean(diag, axis=0)
-    fig, ax = plt.subplots()
-    im = ax.imshow(diag, interpolation='nearest', aspect='auto', vmin=0, vmax=1)
-    ax.set_yticks(range(param_val.shape[-1]))
-    ax.set_yticklabels(param_val[0, :].astype('int'))
-    ax.set_xticks(range(0, time.shape[-1], 25))
-    ax.set_xticklabels(np.squeeze(time[0, 0, ::25]))
-    plt.colorbar(im)
-    #plt.savefig('plot.pdf', bbox_inches='tight')
-    # plt.plot(diag[0, :])
+
+    sim_mat = coef_sim.coef_by_param(exp,
+                                     word,
+                                     sen_type,
+                                     accuracy,
+                                     'B',
+                                     param_specs)
+
+    num_win = sim_mat.shape[0]
+
+    fig, axs = plt.subplots(1, num_win)
+    for i_win in range(num_win):
+        axs[i_win].plot(diag[i_win, :])
+        axs[i_win].plot(sim_mat[i_win, :])
     plt.show()
+
+    # im = ax.imshow(diag, interpolation='nearest', aspect='auto', vmin=0, vmax=1)
+    # ax.set_yticks(range(param_val.shape[-1]))
+    # ax.set_yticklabels(param_val[0, :].astype('int'))
+    # ax.set_xticks(range(0, time.shape[-1], 25))
+    # ax.set_xticklabels(np.squeeze(time[0, 0, ::25]))
+    # plt.colorbar(im)
+    # #plt.savefig('plot.pdf', bbox_inches='tight')
+    # # plt.plot(diag[0, :])
+    # plt.show()
 
 
 
