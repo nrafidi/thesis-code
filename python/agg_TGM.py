975d294d7879b9080717aeea53689bb36b3813b0
nrafidi <nicole.rafidi@gmail.com>
2017-09-29 11:37:30 -0400

    set up plotting to try multiple overlaps

--- python/agg_TGM.py
+++ python/agg_TGM.py
@@ -229,6 +229,55 @@ def get_diag_by_param(result_dict, param_dict, time_dict, param, param_specs, pa
     return diag, param_val, time, starts
 
 
+def get_coef_by_param(exp,
+                  word,
+                  sen_type,
+                  accuracy,
+                  sub,
+                  param_specs):
+    overlap = param_specs['o']
+    sub_results, sub_params, sub_time = agg_results(exp,
+                                                            'coef',
+                                                            word,
+                                                            sen_type,
+                                                            accuracy,
+                                                            sub,
+                                                            param_specs=param_specs)
+
+    sim_mat = np.empty((len(sub_results), len(sub_results[0]) - 1))
+    min_win = 1000
+    for i_win, res in enumerate(sub_results):
+        win_size = int(sub_params['w'][i_win])
+        if len(res) < min_win:
+            min_win = len(res)
+        for i_coef in range(1, len(res)):
+            prev_res = np.zeros(res[i_coef - 1].shape)
+            prev_res[res[i_coef - 1] < 0] = -1.0
+            prev_res[res[i_coef - 1] > 0] = 1.0
+
+            curr_res = np.zeros(res[i_coef].shape)
+            curr_res[res[i_coef] < 0] = -1.0
+            curr_res[res[i_coef] > 0] = 1.0
+
+            prev_mat = np.reshape(np.sum(prev_res, axis=0), (win_size, 306))
+            curr_mat = np.reshape(np.sum(curr_res, axis=0), (win_size, 306))
+
+            if overlap < prev_mat.shape[0]:
+                np.roll(prev_mat, -overlap)
+                end_ind = prev_mat.shape[0] - overlap
+                prev_mat = prev_mat[:end_ind, :]
+                curr_mat = curr_mat[:end_ind, :]
+
+            #         if win_size > overlap:
+            #             f, axs = plt.subplots(2, 1)ddd
+            #             axs[0].imshow(prev_mat, interpolation='nearest', aspect='auto')
+            #             axs[1].imshow(curr_mat, interpolation='nearest', aspect='auto')
+            #              plt.show()
+            sim_mat[i_win, i_coef - 1] = np.linalg.norm(curr_mat - prev_mat)
+    sim_mat = sim_mat[:, 0:(min_win - 1)]
+    return sim_mat
+
+
 if __name__ == '__main__':
     exp = 'krns2'
     mode = 'coef'
--- python/plot_krns2.py
+++ python/plot_krns2.py
@@ -14,67 +14,70 @@ if __name__ == '__main__':
     word = 'firstNoun'
     sen_type = 'active'
     accuracy = 'abs'
-    param_specs = {'w': 50,
-                   'pd': 'F',
-                   'pr': 'F',
-                   'alg': 'LR',
-                   'F': 2,
-                   'z': 'F',
-                   'avg': 'F',
-                   'ni': 2,
-                   'nr': 10,
-                   'rs': 1}
-    sub_results = {}
-    sub_params = {}
-    sub_time = {}
-    for sub in ['B']:  # load_data.VALID_SUBS[exp]:
 
-        sub_results[sub], sub_params[sub], sub_time[sub] = agg_TGM.agg_results(exp,
-                                                                               mode,
-                                                                               word,
-                                                                               sen_type,
-                                                                               accuracy,
-                                                                               sub,
-                                                                               param_specs=param_specs)
-    diag, param_val, time, _ = agg_TGM.get_diag_by_param(sub_results, sub_params, sub_time, 'o', {}, param_limit=50)
-    diag = np.mean(diag, axis=0)
+    for o in [12, 25]:
+        param_specs = {'o': o,
+                       'pd': 'F',
+                       'pr': 'F',
+                       'alg': 'LR',
+                       'F': 2,
+                       'z': 'F',
+                       'avg': 'F',
+                       'ni': 2,
+                       'nr': 10,
+                       'rs': 1}
+        sub_results = {}
+        sub_params = {}
+        sub_time = {}
+        for sub in ['B']:  # load_data.VALID_SUBS[exp]:
 
-    thing_to_plot = 1
-    fig, ax = plt.subplots()
-    ax.plot(diag[thing_to_plot, :])
- #   im = ax.imshow(diag, interpolation='nearest', aspect='auto', vmin=0, vmax=1)
- #   ax.set_yticks(range(param_val.shape[-1]))
-#    ax.set_yticklabels(param_val[0, :].astype('int'))
-    ax.set_xticks(range(0, time.shape[-1], 5))
-    print(time.shape)
-    ax.set_xticklabels(np.squeeze(time[0, thing_to_plot, ::5]))
- #   plt.colorbar(im)
-    #plt.savefig('plot.pdf', bbox_inches='tight')
-    # plt.plot(diag[0, :])
-    plt.show()
+            sub_results[sub], sub_params[sub], sub_time[sub] = agg_TGM.agg_results(exp,
+                                                                                   mode,
+                                                                                   word,
+                                                                                   sen_type,
+                                                                                   accuracy,
+                                                                                   sub,
+                                                                                   param_specs=param_specs)
+        diag, param_val, time, _ = agg_TGM.get_diag_by_param(sub_results, sub_params, sub_time, 'w', {})
+        diag = np.mean(diag, axis=0)
 
-    # sim_mat = coef_sim.coef_by_param(exp,
-    #                                  word,
-    #                                  sen_type,
-    #                                  accuracy,
-    #                                  'B',
-    #                                  param_specs)
-    #
-    # sim_mat = zscore(sim_mat, axis=1)
-    # num_win = sim_mat.shape[0]
-    #
-    # fig, axs = plt.subplots(num_win, 1)
-    # for i_win in range(num_win):
-    #     diag_to_plot = diag[i_win, :]
-    #     diag_to_plot[diag_to_plot > 0.25] = 0.5
-    #     diag_to_plot[diag_to_plot < 0.25] = 0
-    #     axs[i_win].plot(diag_to_plot)
-    #     sim_to_plot = sim_mat[i_win, :]
-    #     sim_to_plot[sim_to_plot > 0] = 0.5
-    #     sim_to_plot[sim_to_plot < 0] = 0
-    #     axs[i_win].plot(sim_to_plot)
-    #     axs[i_win].set_ylim([0, 0.75])
-    # plt.show()
+    #     thing_to_plot = 1
+    #     fig, ax = plt.subplots()
+    #     ax.plot(diag[thing_to_plot, :])
+    #  #   im = ax.imshow(diag, interpolation='nearest', aspect='auto', vmin=0, vmax=1)
+    #  #   ax.set_yticks(range(param_val.shape[-1]))
+    # #    ax.set_yticklabels(param_val[0, :].astype('int'))
+    #     ax.set_xticks(range(0, time.shape[-1], 5))
+    #     print(time.shape)
+    #     ax.set_xticklabels(np.squeeze(time[0, thing_to_plot, ::5]))
+    #  #   plt.colorbar(im)
+    #     #plt.savefig('plot.pdf', bbox_inches='tight')
+    #     # plt.plot(diag[0, :])
+    #     plt.show()
+
+        sim_mat = coef_sim.coef_by_param(exp,
+                                         word,
+                                         sen_type,
+                                         accuracy,
+                                         'B',
+                                         param_specs)
+
+        sim_mat = zscore(sim_mat, axis=1)
+        num_win = sim_mat.shape[0]
+
+        fig, axs = plt.subplots(num_win, 1, figsize=(20, 30))
+        for i_win in range(num_win):
+            diag_to_plot = diag[i_win, :]
+            diag_to_plot[diag_to_plot > 0.25] = 0.5
+            diag_to_plot[diag_to_plot < 0.25] = 0
+            axs[i_win].plot(diag_to_plot)
+            sim_to_plot = sim_mat[i_win, :]
+            sim_to_plot[sim_to_plot > 0] = 0.5
+            sim_to_plot[sim_to_plot < 0] = 0
+            axs[i_win].plot(sim_to_plot)
+            axs[i_win].set_ylim([0, 0.75])
+        fig.suptitle('Overlap {}'.format(o))
+        plt.savefig('overlap{}.pdf'.format(o))
 
 
 
