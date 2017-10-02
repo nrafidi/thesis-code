0ab945a96b74fdb74ccbc3681e1e29343c088419
nrafidi <nicole.rafidi@gmail.com>
2017-10-02 15:46:11 -0400

    adjusting to highlight above-chance points

--- python/plot_krns2.py
+++ python/plot_krns2.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         sub_params = {}
         sub_time = {}
         sub_sim = []
-        for sub in ['B', 'C']: # load_data.VALID_SUBS[exp]:
+        for sub in load_data.VALID_SUBS[exp]:
 
             sub_results[sub], sub_params[sub], sub_time[sub] = agg_TGM.agg_results(exp,
                                                                                    mode,
@@ -49,7 +49,6 @@ if __name__ == '__main__':
         diag = np.mean(diag, axis=0)
 
         sim_mat = np.mean(np.asarray(sub_sim), axis=0)
-        print(sim_mat.shape)
 
         sim_mat -= np.min(sim_mat, axis=1)[:, None]
         sim_mat = np.divide(sim_mat, np.max(sim_mat, axis=1)[:, None])
@@ -67,18 +66,20 @@ if __name__ == '__main__':
             # sim_to_plot[sim_to_plot < 0] = 0.1
             axs[i_win].plot(time_to_plot[:sim_to_plot.shape[0]], sim_to_plot)
             axs[i_win].plot(time_to_plot, np.ones(time_to_plot.shape)*0.25, '--')
+            diag_to_plot[diag_to_plot >= 0.25] = 0.75
+            diag_to_plot[diag_to_plot < 0.25] = 0
+            axs[i_win].plot(time_to_plot, diag_to_plot, ':')
+            new_diag[i_win, :] = diag_to_plot
             axs[i_win].set_ylim([0.1, 1])
             axs[i_win].set_xlim([np.min(plot_time), np.max(plot_time)])
             axs[i_win].set_title(win_len)
-            diag_to_plot[diag_to_plot >= 0.25] = 0.5
-            diag_to_plot[diag_to_plot < 0.25] = 0
-            new_diag[i_win, :] = diag_to_plot
+
         fig.suptitle('Overlap {}'.format(o))
 #        plt.savefig('overlap{}.pdf'.format(o))
         fig, ax = plt.subplots()
         ax.plot(np.sum(new_diag, axis=0))
-        ax.plot(new_diag[-1, :] + 2)
-        ax.plot(new_diag[0, :] + 1)
+        # ax.plot(new_diag[-1, :] + 2)
+        # ax.plot(new_diag[0, :] + 1)
         ax.set_ylim([0, 0.5*num_win + 0.1])
         ax.set_xticks(range(0, time.shape[-1], 25))
         ax.set_xticklabels(np.squeeze(time[0, -1, ::25]))
