c2aa197397ba7a18555db3345f222c3b90a147db
nrafidi <nicole.rafidi@gmail.com>
2017-10-16 15:51:01 -0400

    working on plotting code

--- python/plot_mu_diff.py
+++ python/plot_mu_diff.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
                                                                           accuracy,
                                                                           sub,
                                                                           param_specs=param_specs)
-                mu_diff = sub_results[0][-1]
+                mu_diff = sub_results[0][-1][0]
                 print(mu_diff.shape)
                 # tgm = sub_results[0]
                 # print(tgm.shape)
