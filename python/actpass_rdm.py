fe6a0199dd111c423a097bc16ca75135ba4154b1
nrafidi <nicole.rafidi@gmail.com>
2017-10-18 16:41:11 -0400

    tinkering with plot

--- python/actpass_rdm.py
+++ python/actpass_rdm.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     sorted_inds, sorted_reg = sort_sensors()
 
     evokeds, labels, time, sen_ids = load_data.load_raw(args.subject, word, 'active',
-                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.2)
+                                               experiment=args.experiment, proc=args.proc, tmin=0.05, tmax=0.2)
     act_data, labels_act, sen_ids_act = load_data.avg_data(evokeds, labels, sentence_ids_raw=sen_ids, experiment=args.experiment,
                                               num_instances=args.num_instances, reps_to_use=args.reps_to_use)
     labels_act = np.array(labels_act)
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 
     print(act_data.shape)
     evokeds, labels, _, sen_ids = load_data.load_raw(args.subject, word, 'passive',
-                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.2)
+                                               experiment=args.experiment, proc=args.proc, tmin=0.05, tmax=0.15)
     pass_data, labels_pass, sen_ids_pass = load_data.avg_data(evokeds, labels, sentence_ids_raw=sen_ids, experiment=args.experiment,
                                               num_instances=args.num_instances, reps_to_use=args.reps_to_use)
     labels_pass = np.array(labels_pass)
