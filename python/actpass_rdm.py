9c33596e807273a7befa960d0058da729f3cd496
nrafidi <nicole.rafidi@gmail.com>
2017-10-18 15:20:16 -0400

    adjusting load data args

--- python/actpass_rdm.py
+++ python/actpass_rdm.py
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     rdm = squareform(pdist(total_data))
 
     fig, ax = plt.subplots()
-    h = ax.imshow(rdm, interpolation='nearest', vmin=1.2e-9)
+    h = ax.imshow(rdm, interpolation='nearest')
     plt.colorbar(h)
     plt.show()
