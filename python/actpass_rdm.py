89d5bb233f130c07f29565bea70128f17c445cec
nrafidi <nicole.rafidi@gmail.com>
2017-10-18 15:36:19 -0400

    tinkering with plot

--- python/actpass_rdm.py
+++ python/actpass_rdm.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     sorted_inds, sorted_reg = sort_sensors()
 
     evokeds, labels, time, sen_ids = load_data.load_raw(args.subject, word, 'active',
-                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.5)
+                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.2)
     act_data, labels_act, sen_ids_act = load_data.avg_data(evokeds, labels, sentence_ids_raw=sen_ids, experiment=args.experiment,
                                               num_instances=args.num_instances, reps_to_use=args.reps_to_use)
     labels_act = np.array(labels_act)
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 
     print(act_data.shape)
     evokeds, labels, _, sen_ids = load_data.load_raw(args.subject, word, 'passive',
-                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.5)
+                                               experiment=args.experiment, proc=args.proc, tmin=0.0, tmax=0.2)
     pass_data, labels_pass, sen_ids_pass = load_data.avg_data(evokeds, labels, sentence_ids_raw=sen_ids, experiment=args.experiment,
                                               num_instances=args.num_instances, reps_to_use=args.reps_to_use)
     labels_pass = np.array(labels_pass)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         rdm = squareform(pdist(reshaped_data))
 
         fig, ax = plt.subplots()
-        h = ax.imshow(rdm, interpolation='nearest', vmin=5e-11, vmax=2e-10)
+        h = ax.imshow(rdm, interpolation='nearest', vmin=0, vmax=3e-10)
         ax.set_title(reg)
         plt.colorbar(h)
     plt.show()
