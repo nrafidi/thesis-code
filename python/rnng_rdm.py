5582ca99397f542e75a76c15e6b0834834b5ade9
Nicole <nicole.rafidi@gmail.com>
2017-10-23 10:27:20 -0400

    approx semantic rdm out of curiosity

--- python/rnng_rdm.py
+++ python/rnng_rdm.py
@@ -14,7 +14,8 @@ NUMAP = 96
 WAS = 'was'
 BY = 'by'
 
-SEN_LENS = {'A': 5, 'P': 7, 'AS': 4, }
+SEN_LENS = {6: 'A', 8: 'P', 4: 'AS', 5: 'PS'}
+KEY_WORDS = {'A': [1, 2, 4], 'P': [1, 3, 6], 'AS': [1, 2], 'PS': [1, 3]}
 
 
 
@@ -34,24 +35,26 @@ def syn_rdm(ap_list):
                 ap_rdm[i, j] = 1.0
     return ap_rdm
 
-# def sem_rdm(sen_list):
-#     ap_rdm = np.empty((NUMAP, NUMAP))
-#     for i, i_sen in enumerate(ap_list):
-#         if i >= NUMAP:
-#             break
-#         words_i = string.split(i_sen)
-#         if len(words_i) ==
-#         for j, j_sen in enumerate(ap_list):
-#             if j >= NUMAP:
-#                 break
-#
-#             if i_sen == j_sen:
-#                 ap_rdm[i, j] = 0.0
-#             elif i_sen in j_sen or j_sen in i_sen:
-#                 ap_rdm[i, j] = 0.5
-#             else:
-#                 ap_rdm[i, j] = 1.0
-#     return ap_rdm
+
+def sem_rdm(sen_list):
+    ap_rdm = np.empty((NUMAP, NUMAP))
+    for i, i_sen in enumerate(sen_list):
+        if i >= NUMAP:
+            break
+        key_words_i = [i_sen[i_word] for i_word in KEY_WORDS[SEN_LENS[len(i_sen)]]]
+        for j, j_sen in enumerate(sen_list):
+            if j >= NUMAP:
+                break
+            key_words_j = [j_sen[j_word] for j_word in KEY_WORDS[SEN_LENS[len(j_sen)]]]
+            if i_sen == j_sen:
+                ap_rdm[i, j] = 0.0
+            elif key_words_i == key_words_j.reverse():
+                ap_rdm[i, j] = 0.0
+            elif key_words_i[0] == key_words_j[0] and key_words_i[1] == key_words_j[1]:
+                ap_rdm[i, j] = 0.5
+            else:
+                ap_rdm[i, j] = 1.0
+    return ap_rdm
 
 
 if __name__ == '__main__':
@@ -61,9 +64,10 @@ if __name__ == '__main__':
     vec_rdm = squareform(pdist(vectors))
 
     ap_list = []
+    sen_list = []
     with open(SENTENCES) as f:
         for line in f:
-            print(string.split(line))
+            sen_list.append(string.split(line))
             if WAS in line:
                 if BY in line:
                     ap_list.append('P')
@@ -76,14 +80,23 @@ if __name__ == '__main__':
                     ap_list.append('A')
 
     ap_rdm = syn_rdm(ap_list)
+    semantic_rdm = sem_rdm(sen_list)
 
     ktau, pval = kendalltau(vec_rdm, ap_rdm)
 
-    print('Kendall tau is {} with pval {}'.format(ktau, pval))
+    print('Syntactic Kendall tau is {} with pval {}'.format(ktau, pval))
+
+    ktau, pval = kendalltau(vec_rdm, semantic_rdm)
+
+    print('Semantic Kendall tau is {} with pval {}'.format(ktau, pval))
 
     r, p, z = Mantel.test(vec_rdm, ap_rdm)
 
-    print('Pearson is {} with pval {} and zval {} from Mantel test'.format(r, p, z))
+    print('Syntactic Pearson is {} with pval {} and zval {} from Mantel test'.format(r, p, z))
+
+    r, p, z = Mantel.test(vec_rdm, semantic_rdm)
+
+    print('Semantic Pearson is {} with pval {} and zval {} from Mantel test'.format(r, p, z))
 
     fig, ax = plt.subplots()
     h = ax.imshow(vec_rdm, interpolation='nearest')
@@ -95,6 +108,10 @@ if __name__ == '__main__':
     plt.colorbar(h)
     ax.set_title('Approx Syntax RDM over Active and Passive Sentences')
     fig.savefig('syntax_rdm_actpass.pdf', bbox_inches='tight')
-
-    plt.show()
+    fig, ax = plt.subplots()
+    h = ax.imshow(semantic_rdm, interpolation='nearest')
+    plt.colorbar(h)
+    ax.set_title('Approx Semantic RDM over Active and Passive Sentences')
+    fig.savefig('semantic_rdm_actpass.pdf', bbox_inches='tight')
+    # plt.show()
 
