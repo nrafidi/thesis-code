3d597647ac4ee6e748b0ba247f47f2869574a2d3
nrafidi <nicole.rafidi@gmail.com>
2017-10-30 15:21:59 -0400

    summarize over sensor locs

--- python/agg_TGM.py
+++ python/agg_TGM.py
@@ -119,9 +119,7 @@ def comb_over_reg(preds):
     new_preds = np.empty((s0, s1, len(uni_reg), s3))
     print(new_preds.shape)
     for i_reg, reg in enumerate(uni_reg):
-        print(reg)
-        print(sorted_reg == reg)
-        inds_to_sum = sorted_inds[np.where(sorted_reg == reg)]
+        inds_to_sum = [sorted_inds[j] for j, j_reg in enumerate(sorted_reg) if j_reg == reg]
         print(inds_to_sum)
         meow = np.sum(preds[:, :, inds_to_sum, :], axis=2)
         print(meow.shape)
