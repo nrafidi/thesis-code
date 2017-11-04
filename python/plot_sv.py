df6f4c5e0aa13349f7a9c554d582514c2c0ba8d8
Nicole <nicole.rafidi@gmail.com>
2017-11-04 11:34:47 -0400

    plotting generative model results

--- python/plot_sv.py
+++ python/plot_sv.py
@@ -80,8 +80,7 @@ if __name__ == '__main__':
     best_score = best_score[sorted_inds, :]
 
     fig, ax = plt.subplots()
-    h = ax.imshow(best_score, interpolation='nearest', aspect='auto')
-    ax.set_ylim(0.0, 0.01)
+    h = ax.imshow(best_score, interpolation='nearest', aspect='auto', vmin=0.0, vmax=0.01)
     ax.set_yticks(yticks_sens)
     ax.set_yticklabels(uni_reg)
     ax.set_ylabel('Sensors')
