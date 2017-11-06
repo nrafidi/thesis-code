db273358a15fc9d07759f2f4cfc8584c923a1676
Nicole <nicole.rafidi@gmail.com>
2017-11-06 08:05:45 -0500

    updated to use bhy + mantel test

--- python/rnng_v_lstm_rdm.py
+++ python/rnng_v_lstm_rdm.py
@@ -11,10 +11,11 @@ from scipy.stats import kendalltau
 import rnng_rdm
 import os.path
 import pickle
+import Mantel
 
 SENSOR_MAP = '/bigbrain/bigbrain.usr1/homes/nrafidi/MATLAB/groupRepo/shared/megVis/sensormap.mat'
 SAVE_RDM = '/share/volume0/nrafidi/RDM_{exp}_{tmin}_{tmax}_{word}.npz'
-SAVE_SCORES = '/share/volume0/nrafidi/Scores_{exp}_{metric}_{reg}_{tmin}_{tmax}_{word}_semantics_rnng_lstm_corr.npz'
+SAVE_SCORES = '/share/volume0/nrafidi/Scores_{exp}_{metric}_{reg}_{tmin}_{tmax}_{word}_rnng_lstm_mantel.npz'
 VECTORS = '/share/volume0/RNNG/sentence_stimuli_tokenized_tagged_pred_trees_no_preterms_vectors.txt'
 LSTM = '/share/volume0/RNNG/test_sents_vectors_lstm.txt'
 
@@ -33,49 +34,6 @@ def sort_sensors():
     sorted_inds = np.argsort(sensor_reg)
     sorted_reg = [sensor_reg[ind] for ind in sorted_inds]
     return sorted_inds, sorted_reg
-
-
-def word_len_rdm(words):
-    print(words)
-    num_words = words.size
-    rdm = np.zeros((num_words, num_words))
-    for i in range(num_words):
-        for j in range(num_words):
-            rdm[i, j] = np.abs(len(words[i]) - len(words[j]))
-    return rdm
-
-def word_id_rdm(words):
-    print(words)
-    num_words = words.size
-    rdm = np.zeros((num_words, num_words))
-    for i in range(num_words):
-        for j in range(num_words):
-            if words[i] == words[j]:
-                rdm[i, j] = 0
-            else:
-                rdm[i, j] = 1
-    return rdm
-
-
-def ani_rdm(words):
-    print(words)
-    num_words = words.size
-    rdm = np.zeros((num_words, num_words))
-    for i in range(num_words):
-        wordi = words[i]
-        if '.' in wordi:
-            wordi = wordi[:-1]
-        for j in range(num_words):
-            wordj = words[j]
-            if '.' in wordj:
-                wordj = wordj[:-1]
-            if wordi in ANIMATE and wordj in ANIMATE:
-                rdm[i, j] = 0
-            elif wordi in INANIMATE and wordj in INANIMATE:
-                rdm[i, j] = 0
-            else:
-                rdm[i, j] = 1
-    return rdm
 
 
 def rank_correlate_rdms(rdm1, rdm2):
@@ -103,6 +61,33 @@ def load_sentence_data(subject, word, sen_type, experiment, proc, num_instances,
         data = data[:, sorted_inds, :]
 
     return data, labels, time
+
+
+def bhy_multiple_comparisons_procedure(uncorrected_pvalues, alpha=0.05):
+    # originally from Mariya Toneva
+    if len(uncorrected_pvalues.shape) == 1:
+        uncorrected_pvalues = np.reshape(uncorrected_pvalues, (1, -1))
+
+    # get ranks of all p-values in ascending order
+    sorting_inds = np.argsort(uncorrected_pvalues, axis=1)
+    ranks = sorting_inds + 1  # add 1 to make the ranks start at 1 instead of 0
+
+    # calculate critical values under arbitrary dependence
+    dependency_constant = np.sum(1 / ranks)
+    critical_values = ranks * alpha / (uncorrected_pvalues.shape[1] * dependency_constant)
+
+    # find largest pvalue that is <= than its critical value
+    sorted_pvalues = uncorrected_pvalues[:, sorting_inds]
+    sorted_critical_values = critical_values[:, sorting_inds]
+    bh_thresh = np.empty((sorted_pvalues.shape[0],))
+    for j in range(sorted_pvalues.shape[0]):
+        for i in range(sorted_pvalues.shape[1] - 1, -1, -1):  # start from the back
+            if sorted_pvalues[j, i] <= sorted_critical_values[j, i]:
+                bh_thresh[j] = sorted_pvalues[j, i]
+                print('threshold for row ', j, ' is:', bh_thresh, 'critical value:', sorted_critical_values[j, i], i)
+                break
+
+    return bh_thresh
 
 
 if __name__ == '__main__':
@@ -182,171 +167,115 @@ if __name__ == '__main__':
     num_reg = rdm.shape[0]
 
     fig, axs = plt.subplots(num_reg, 1, figsize=(20, 20))
-    fig_zoom, axs_zoom = plt.subplots(num_reg, 1, figsize=(20, 20))
+    fig_mantel, axs_mantel = plt.subplots(num_reg, 1, figsize=(20, 20))
+
     time = np.arange(args.tmin, args.tmax+0.002, 0.002)
     time_zoom = np.arange(0.0, args.tmax + 0.002, 0.002)
     min_reg = np.empty((num_reg,))
     max_reg = np.empty((num_reg,))
-    min_reg_zoom = np.empty((num_reg,))
-    max_reg_zoom = np.empty((num_reg,))
+    min_reg_mantel = np.empty((num_reg,))
+    max_reg_mantel = np.empty((num_reg,))
     colors = ['b', 'g', 'r', 'c']
     for i_reg in range(num_reg):
         print(uni_reg[i_reg])
         ax = axs[i_reg]
-        ax_zoom = axs_zoom[i_reg]
+        ax_mantel = axs_mantel[i_reg]
         fname = SAVE_SCORES.format(exp=args.experiment, metric=args.dist, reg=uni_reg[i_reg], tmin=args.tmin, tmax=args.tmax, word=args.word)
         if os.path.isfile(fname):
             result = np.load(fname)
-            rnng_scores = result['rnng_scores']
-            lstm_scores = result['lstm_scores']
+            rnng_scores_rank = result['rnng_scores_rank']
+            lstm_scores_rank = result['lstm_scores_rank']
+            rnng_scores_mantel = result['rnng_scores_mantel']
+            lstm_scores_mantel = result['lstm_scores_mantel']
+            rnng_pvals_mantel = result['rnng_pvals_mantel']
+            lstm_pvals_mantel = result['lstm_pvals_mantel']
         else:
-            raise NameError('need to rerun score calculation')
+            rnng_scores_rank = np.empty((rdm.shape[1],))
+            lstm_scores_rank = np.empty((rdm.shape[1],))
+            rnng_scores_mantel = np.empty((rdm.shape[1],))
+            lstm_scores_mantel = np.empty((rdm.shape[1],))
+            rnng_pvals_mantel = np.empty((rdm.shape[1],))
+            lstm_pvals_mantel = np.empty((rdm.shape[1],))
+            for i_t in range(rdm.shape[1]):
+                rnng_scores_rank[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), vec_rdm)
+                lstm_scores_rank[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), lstm_rdm)
+                rnng_scores_mantel[i_t], rnng_pvals_mantel[i_t], _ = Mantel.test(np.squeeze(rdm[i_reg, i_t, :, :]), vec_rdm)
+                lstm_scores_mantel[i_t], lstm_pvals_mantel[i_t], _ = Mantel.test(np.squeeze(rdm[i_reg, i_t, :, :]), lstm_rdm)
+            np.savez_compressed(fname, rnng_scores_rank=rnng_scores_rank, lstm_scores_rank=lstm_scores_rank,
+                                rnng_scores_mantel=rnng_scores_mantel, lstm_scores_mantel=lstm_scores_mantel,
+                                rnng_pvals_mantel=rnng_pvals_mantel, lstm_pvals_mantel=lstm_pvals_mantel)
 
-        min_reg[i_reg] = np.min([np.min(rnng_scores), np.min(lstm_scores)])
-        max_reg[i_reg] = np.max([np.max(rnng_scores), np.max(lstm_scores)])
+        min_reg[i_reg] = np.min([np.min(rnng_scores_rank), np.min(lstm_scores_rank)])
+        max_reg[i_reg] = np.max([np.max(rnng_scores_rank), np.max(lstm_scores_rank)])
+        min_reg_mantel[i_reg] = np.min([np.min(rnng_scores_mantel), np.min(lstm_scores_mantel)])
+        max_reg_mantel[i_reg] = np.max([np.max(rnng_scores_mantel), np.max(lstm_scores_mantel)])
 
-        all_scores = np.concatenate([rnng_scores[None, ...], lstm_scores[None, ...], ])
-        print(all_scores.shape)
+        all_scores_rank = np.concatenate([rnng_scores_rank[None, ...], lstm_scores_rank[None, ...], ])
+        print(all_scores_rank.shape)
+        all_pvals_mantel = np.concatenate([rnng_pvals_mantel[None, ...], lstm_pvals_mantel[None, ...], ])
+        print(all_pvals_mantel.shape)
 
-        good_scores = all_scores >= 0.15
-        print(good_scores.shape)
+        good_scores_rank = all_scores_rank >= 0.15
+        print(good_scores_rank.shape)
+        bh_threshs = bhy_multiple_comparisons_procedure(all_pvals_mantel)
+        print(bh_threshs)
+        good_pvals_mantel = np.concatenate(all_pvals_mantel[0, :] <= bh_threshs[0], all_pvals_mantel[1, :] <= bh_threshs[1])
 
-        win_scores = np.argmax(all_scores, axis=0)
-        print(win_scores.shape)
+        win_scores_rank = np.argmax(all_scores_rank, axis=0)
+        print(win_scores_rank.shape)
+        win_scores_mantel = np.argmin(all_pvals_mantel, axis=0)
 
 
+        h0 = ax_mantel.plot(time, rnng_scores_mantel)
+        h1 = ax_mantel.plot(time, lstm_scores_mantel)
+        h0[0].set_label('RNNG')
+        h1[0].set_label('LSTM')
+        ax_mantel.legend()
 
-        h5 = ax.plot(time, rnng_scores)
-        h6 = ax.plot(time, lstm_scores)
+        for i_time in range(all_pvals_mantel.shape[-1]):
+            if good_pvals_mantel[win_scores_mantel[i_time], i_time]:
+                ax_mantel.scatter(time[i_time], all_pvals_mantel[win_scores_mantel[i_time], i_time]+0.05,
+                           c=colors[win_scores_mantel[i_time]], linewidths=0.0)
+        ax_mantel.set_title(uni_reg[i_reg])
+        ax_mantel.set_xlim(args.tmin, args.tmax+0.5)
+        ax_mantel.set_xticks(np.arange(args.tmin, args.tmax, 0.5))
 
+
+        h5 = ax.plot(time, rnng_scores_rank)
+        h6 = ax.plot(time, lstm_scores_rank)
         h5[0].set_label('RNNG')
         h6[0].set_label('LSTM')
         ax.legend()
 
-        for i_time in range(all_scores.shape[-1]):
-            if good_scores[win_scores[i_time], i_time]:
-                ax.scatter(time[i_time], all_scores[win_scores[i_time], i_time]+0.05, c=colors[win_scores[i_time]], linewidths=0.0)
+        for i_time in range(all_scores_rank.shape[-1]):
+            if good_scores_rank[win_scores_rank[i_time], i_time]:
+                ax.scatter(time[i_time], all_scores_rank[win_scores_rank[i_time], i_time]+0.05,
+                           c=colors[win_scores_rank[i_time]], linewidths=0.0)
         ax.set_title(uni_reg[i_reg])
         ax.set_xlim(args.tmin, args.tmax+0.5)
         ax.set_xticks(np.arange(args.tmin, args.tmax, 0.5))
 
-        rnng_scores_zoom = rnng_scores[time >= 0.0]
-        lstm_scores_zoom = lstm_scores[time >= 0.0]
-        min_reg_zoom[i_reg] = np.min(
-            [np.min(rnng_scores_zoom), np.min(lstm_scores_zoom)])
-        max_reg_zoom[i_reg] = np.max(
-            [np.max(rnng_scores_zoom), np.max(lstm_scores_zoom)])
 
-        all_scores_zoom = np.concatenate(
-            [rnng_scores_zoom[None, ...], lstm_scores_zoom[None, ...], ])
-
-        good_scores_zoom = all_scores_zoom>= 0.15
-
-        win_scores_zoom = np.argmax(all_scores_zoom, axis=0)
-
-        h5 = ax_zoom.plot(time_zoom, rnng_scores_zoom)
-        h6 = ax_zoom.plot(time_zoom, lstm_scores_zoom)
-
-        h5[0].set_label('RNNG')
-        h6[0].set_label('LSTM')
-        ax_zoom.legend()
-
-        for i_time in range(all_scores_zoom.shape[-1]):
-            if good_scores_zoom[win_scores_zoom[i_time], i_time]:
-                ax_zoom.scatter(time_zoom[i_time], all_scores_zoom[win_scores_zoom[i_time], i_time]+0.05, c=colors[win_scores_zoom[i_time]], linewidths=0.0)
-
-        ax_zoom.set_title(uni_reg[i_reg])
-        ax_zoom.set_xlim(0.0, args.tmax + 0.5)
-        ax_zoom.set_xticks(np.arange(0.0, args.tmax, 0.5))
-
-        # ax.legend([h1, h2], ['Syntax', 'Semantics'])
     max_val = np.max(max_reg)
     min_val = np.min(min_reg)
     for i_reg in range(num_reg):
         axs[i_reg].set_ylim(min_val, max_val+0.1)
-    max_val_zoom = np.max(max_reg_zoom)
-    min_val_zoom = np.min(min_reg_zoom)
+
+    max_val_mantel = np.max(max_reg_mantel)
+    min_val_mantel = np.min(min_reg_mantel)
     for i_reg in range(num_reg):
-        axs_zoom[i_reg].set_ylim(min_val_zoom, max_val_zoom+0.1)
+        axs_mantel[i_reg].set_ylim(min_val_mantel, max_val_mantel + 0.1)
 
-    fig.suptitle('{} {}'.format(args.experiment, args.dist))
+
+    fig.suptitle('{} {} kendall'.format(args.experiment, args.dist))
     fig.tight_layout()
-    fig_zoom.suptitle('{} {}'.format(args.experiment, args.dist))
-    fig_zoom.tight_layout()
-    fig.savefig('RDM_scores_RvL_{exp}_{metric}_{tmin}_{tmax}_{word}.pdf'.format(exp=args.experiment, metric=args.dist, tmin=args.tmin, tmax=args.tmax, word=args.word))
-    fig_zoom.savefig('RDM_scores_RvL_{exp}_{metric}_0_{tmax}_{word}.pdf'.format(exp=args.experiment, metric=args.dist,
-                                                                                 tmax=args.tmax,
-                                                                                 word=args.word))
-    # plt.savefig()
+
+    fig_mantel.suptitle('{} {} Mantel'.format(args.experiment, args.dist))
+    fig_mantel.tight_layout()
+
+    fig.savefig('RDM_kendall_scores_RvL_{exp}_{metric}_{tmin}_{tmax}_{word}.pdf'.format(exp=args.experiment, metric=args.dist, tmin=args.tmin, tmax=args.tmax, word=args.word))
+    fig_mantel.savefig('RDM_mantel_scores_RvL_{exp}_{metric}_{tmin}_{tmax}_{word}.pdf'.format(exp=args.experiment, metric=args.dist,
+                                                                                tmin=args.tmin, tmax=args.tmax,
+                                                                                word=args.word))
+
     plt.show()
-
-
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #     best_rdm_len = np.argmax(score_rdm_len)
-    #     fig, ax = plt.subplots()
-    #     h = ax.imshow(rdm_list[best_rdm_len], interpolation='nearest', aspect='auto')
-    #     plt.colorbar(h)
-    #     ax.set_title('{} {} {} {} len'.format(reg, time_act[best_rdm_len],
-    #                                    score_rdm_len[best_rdm_len], word))
-    #     plt.savefig('RDM_len_{}_{}_{}.pdf'.format(reg, best_rdm_len, word))
-    #
-    #     fig, ax = plt.subplots()
-    #     ax.plot(time_act, score_rdm_len)
-    #     ax.set_title('{} {} len'.format(reg, word))
-    #     ax.set_ylim(0, 0.5)
-    #     plt.savefig('Score_len_{}_{}.pdf'.format(reg, word))
-    #
-    #     best_rdm_id = np.argmax(score_rdm_id)
-    #     fig, ax = plt.subplots()
-    #     h = ax.imshow(rdm_list[best_rdm_id], interpolation='nearest', aspect='auto')
-    #     plt.colorbar(h)
-    #     ax.set_title('{} {} {} {} id'.format(reg, time_act[best_rdm_id],
-    #                                    score_rdm_id[best_rdm_id], word))
-    #     plt.savefig('RDM_id_{}_{}_{}.pdf'.format(reg, best_rdm_id, word))
-    #
-    #     fig, ax = plt.subplots()
-    #     ax.plot(time_act, score_rdm_id)
-    #     ax.set_ylim(0, 0.5)
-    #     ax.set_title('{} {} id'.format(reg, word))
-    #     plt.savefig('Score_id_{}_{}.pdf'.format(reg, word))
-    #
-    #     best_rdm_ani = np.argmax(score_rdm_ani)
-    #     fig, ax = plt.subplots()
-    #     h = ax.imshow(rdm_list[best_rdm_ani], interpolation='nearest', aspect='auto')
-    #     plt.colorbar(h)
-    #     ax.set_title('{} {} {} {} ani'.format(reg, time_act[best_rdm_ani],
-    #                                          score_rdm_id[best_rdm_ani], word))
-    #     plt.savefig('RDM_ani_{}_{}_{}.pdf'.format(reg, best_rdm_ani, word))
-    #
-    # fig, ax = plt.subplots()
-    # ax.plot(time_act, score_rdm_ani)
-    # ax.set_ylim(0, 0.5)
-    # ax.set_title('{} {} ani'.format(reg, word))
-    # plt.savefig('Score_ani_{}_{}.pdf'.format(reg, word))
-    # word_rdm_len = word_len_rdm(total_labels)
-    # fig, ax = plt.subplots()
-    # h = ax.imshow(word_rdm_len, interpolation='nearest')
-    # ax.set_title('Word len RDM')
-    # plt.colorbar(h)
-    # plt.savefig('RDM_word_len_{}.pdf'.format(word))
-    #
-    # word_rdm_id = word_id_rdm(total_labels)
-    # fig, ax = plt.subplots()
-    # h = ax.imshow(word_rdm_id, interpolation='nearest')
-    # ax.set_title('Word id RDM')
-    # plt.colorbar(h)
-    # plt.savefig('RDM_word_id_{}.pdf'.format(word))
-    #
-    # word_rdm_ani = ani_rdm(total_labels)
-    # fig, ax = plt.subplots()
-    # h = ax.imshow(word_rdm_ani, interpolation='nearest')
-    # ax.set_title('Word ani RDM')
-    # plt.colorbar(h)
-    # plt.savefig('RDM_word_ani_{}.pdf'.format(word))
-    # # plt.show()
