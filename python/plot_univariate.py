085982b547a6e160ca1d020b4fab1132485ebbb8
Nicole <nicole.rafidi@gmail.com>
2017-11-07 10:55:57 -0500

    plot univariate nvn

--- python/plot_univariate.py
+++ python/plot_univariate.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
         for i in range(word_tgm.shape[2]):
             for j in range(word_tgm.shape[3]):
                 sub_perf = np.sum(word_tgm[:, :, i, j] >= 0.3, axis=1)
-                if np.any(sub_perf > 5):
+                if np.any(sub_perf > 4):
                     total_best[i, j, np.argmax(avg_tgm[:, i, j])] = 1
                 else:
                     total_best[i, j, :] = [0.8, 0.8, 0.8]
