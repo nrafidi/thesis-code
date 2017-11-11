c5d8d5e7f52a2d739e11b803089fc2141e0a8d5a
Nicole <nicole.rafidi@gmail.com>
2017-11-11 00:16:38 -0500

    rerun perms for subjects G and H

--- scripts/batch_experiments.py
+++ scripts/batch_experiments.py
@@ -22,7 +22,7 @@ import time
 #parser.add_argument('--random_state', type=int, default=1)
 
 EXPERIMENTS = ['krns2']  # ,  'PassAct2', 'PassAct3']
-SUBJECTS = ['B', 'C', 'D', 'E', 'F', 'G', 'H']
+SUBJECTS = ['G', 'H', 'B', 'C', 'D', 'E', 'F']
 SEN_TYPES = ['active', 'passive'] #, 'active']
 WORDS = ['firstNoun', 'verb', 'secondNoun']
 WIN_LENS = [-1] #-1, 3, 6, 12, 25] #, 2000]
