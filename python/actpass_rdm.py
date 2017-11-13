f056c7b3d00ecc5244869ccdf85d9f9d1613b959
nrafidi <nicole.rafidi@gmail.com>
2017-11-13 14:18:31 -0500

    plot subset of regions

--- python/actpass_rdm.py
+++ python/actpass_rdm.py
@@ -220,108 +220,108 @@ if __name__ == '__main__':
     min_reg_zoom = np.empty((num_reg,))
     max_reg_zoom = np.empty((num_reg,))
     colors = ['b', 'g', 'r', 'c']
-    for i_reg in range(num_reg):
-        print(uni_reg[i_reg])
-        if uni_reg[i_reg] in REGIONS_TO_PLOT:
-            ax = axs[i_reg]
-            ax_zoom = axs_zoom[i_reg]
-            fname = SAVE_SCORES.format(exp=args.experiment, metric=args.dist, reg=uni_reg[i_reg], tmin=args.tmin, tmax=args.tmax, word=args.word)
-            if os.path.isfile(fname):
-                result = np.load(fname)
-                syn_scores = result['syn_scores']
-                sem_scores = result['sem_scores']
-                glove_scores = result['glove_scores']
-                w2v_scores = result['w2v_scores']
-                rnng_scores = result['rnng_scores']
-                lstm_scores = result['lstm_scores']
-            else:
-                syn_scores = np.empty((rdm.shape[1],))
-                sem_scores = np.empty((rdm.shape[1],))
-                glove_scores = np.empty((rdm.shape[1],))
-                w2v_scores = np.empty((rdm.shape[1],))
-                rnng_scores = np.empty((rdm.shape[1],))
-                lstm_scores = np.empty((rdm.shape[1],))
-                for i_t in range(rdm.shape[1]):
-                    syn_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), ap_rdm)
-                    sem_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), semantic_rdm)
-                    glove_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), glove_rdm)
-                    w2v_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), w2v_rdm)
-                    rnng_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), vec_rdm)
-                    lstm_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[i_reg, i_t, :, :]), lstm_rdm)
-                np.savez_compressed(fname, syn_scores=syn_scores, sem_scores=sem_scores, glove_scores=glove_scores,
-                                    w2v_scores=w2v_scores, rnng_scores=rnng_scores, lstm_scores=lstm_scores)
+    for i_reg, reg in enumerate(REGIONS_TO_PLOT):
+        j_reg = uni_reg.index(reg)
+        print(uni_reg[j_reg])
+        ax = axs[i_reg]
+        ax_zoom = axs_zoom[i_reg]
+        fname = SAVE_SCORES.format(exp=args.experiment, metric=args.dist, reg=uni_reg[j_reg], tmin=args.tmin, tmax=args.tmax, word=args.word)
+        if os.path.isfile(fname):
+            result = np.load(fname)
+            syn_scores = result['syn_scores']
+            sem_scores = result['sem_scores']
+            glove_scores = result['glove_scores']
+            w2v_scores = result['w2v_scores']
+            rnng_scores = result['rnng_scores']
+            lstm_scores = result['lstm_scores']
+        else:
+            syn_scores = np.empty((rdm.shape[1],))
+            sem_scores = np.empty((rdm.shape[1],))
+            glove_scores = np.empty((rdm.shape[1],))
+            w2v_scores = np.empty((rdm.shape[1],))
+            rnng_scores = np.empty((rdm.shape[1],))
+            lstm_scores = np.empty((rdm.shape[1],))
+            for i_t in range(rdm.shape[1]):
+                syn_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), ap_rdm)
+                sem_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), semantic_rdm)
+                glove_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), glove_rdm)
+                w2v_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), w2v_rdm)
+                rnng_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), vec_rdm)
+                lstm_scores[i_t], _ = rank_correlate_rdms(np.squeeze(rdm[j_reg, i_t, :, :]), lstm_rdm)
+            np.savez_compressed(fname, syn_scores=syn_scores, sem_scores=sem_scores, glove_scores=glove_scores,
+                                w2v_scores=w2v_scores, rnng_scores=rnng_scores, lstm_scores=lstm_scores)
 
-            min_reg[i_reg] = np.min([np.min(syn_scores), np.min(glove_scores), np.min(rnng_scores), np.min(lstm_scores)])
-            max_reg[i_reg] = np.max([np.max(syn_scores), np.max(glove_scores), np.max(rnng_scores), np.max(lstm_scores)])
+        min_reg[i_reg] = np.min([np.min(syn_scores), np.min(glove_scores), np.min(rnng_scores), np.min(lstm_scores)])
+        max_reg[i_reg] = np.max([np.max(syn_scores), np.max(glove_scores), np.max(rnng_scores), np.max(lstm_scores)])
 
-            all_scores = np.concatenate([syn_scores[None, ...], glove_scores[None, ...], rnng_scores[None, ...], lstm_scores[None, ...], ])
-            print(all_scores.shape)
+        all_scores = np.concatenate([syn_scores[None, ...], glove_scores[None, ...], rnng_scores[None, ...], lstm_scores[None, ...], ])
+        print(all_scores.shape)
 
-            good_scores = all_scores >= 0.15
-            print(good_scores.shape)
+        good_scores = all_scores >= 0.15
+        print(good_scores.shape)
 
-            win_scores = np.argmax(all_scores, axis=0)
-            print(win_scores.shape)
+        win_scores = np.argmax(all_scores, axis=0)
+        print(win_scores.shape)
 
 
-            h1 = ax.plot(time, syn_scores)
-            # h2 = ax.plot(time, sem_scores)
-            h3 = ax.plot(time, glove_scores)
-            # h4 = ax.plot(time, w2v_scores)
-            h5 = ax.plot(time, rnng_scores)
-            h6 = ax.plot(time, lstm_scores)
-            h1[0].set_label('Syntax')
-            # h2[0].set_label('Simple Semantics')
-            h3[0].set_label('glove Semantics')
-            # h4[0].set_label('w2v Semantics')
-            h5[0].set_label('RNNG')
-            h6[0].set_label('LSTM')
-            ax.legend()
+        h1 = ax.plot(time, syn_scores)
+        # h2 = ax.plot(time, sem_scores)
+        h3 = ax.plot(time, glove_scores)
+        # h4 = ax.plot(time, w2v_scores)
+        h5 = ax.plot(time, rnng_scores)
+        h6 = ax.plot(time, lstm_scores)
+        h1[0].set_label('Syntax')
+        # h2[0].set_label('Simple Semantics')
+        h3[0].set_label('glove Semantics')
+        # h4[0].set_label('w2v Semantics')
+        h5[0].set_label('RNNG')
+        h6[0].set_label('LSTM')
+        ax.legend()
 
-            # for i_time in range(all_scores.shape[-1]):
-            #     if good_scores[win_scores[i_time], i_time]:
-            #         ax.scatter(time[i_time], all_scores[win_scores[i_time], i_time]+0.05, c=colors[win_scores[i_time]], linewidths=0.0)
-            ax.set_title(uni_reg[i_reg])
-            ax.set_xlim(args.tmin, args.tmax+0.5)
-            ax.set_xticks(np.arange(args.tmin, args.tmax, 0.5))
+        # for i_time in range(all_scores.shape[-1]):
+        #     if good_scores[win_scores[i_time], i_time]:
+        #         ax.scatter(time[i_time], all_scores[win_scores[i_time], i_time]+0.05, c=colors[win_scores[i_time]], linewidths=0.0)
+        ax.set_title(uni_reg[i_reg])
+        ax.set_xlim(args.tmin, args.tmax+0.5)
+        ax.set_xticks(np.arange(args.tmin, args.tmax, 0.5))
 
-            syn_scores_zoom = syn_scores[time >= 0.0]
-            glove_scores_zoom = glove_scores[time >= 0.0]
-            rnng_scores_zoom = rnng_scores[time >= 0.0]
-            lstm_scores_zoom = lstm_scores[time >= 0.0]
-            min_reg_zoom[i_reg] = np.min(
-                [np.min(syn_scores_zoom), np.min(glove_scores_zoom), np.min(rnng_scores_zoom), np.min(lstm_scores_zoom)])
-            max_reg_zoom[i_reg] = np.max(
-                [np.max(syn_scores_zoom), np.max(glove_scores_zoom), np.max(rnng_scores_zoom), np.max(lstm_scores_zoom)])
+        syn_scores_zoom = syn_scores[time >= 0.0]
+        glove_scores_zoom = glove_scores[time >= 0.0]
+        rnng_scores_zoom = rnng_scores[time >= 0.0]
+        lstm_scores_zoom = lstm_scores[time >= 0.0]
+        min_reg_zoom[i_reg] = np.min(
+            [np.min(syn_scores_zoom), np.min(glove_scores_zoom), np.min(rnng_scores_zoom), np.min(lstm_scores_zoom)])
+        max_reg_zoom[i_reg] = np.max(
+            [np.max(syn_scores_zoom), np.max(glove_scores_zoom), np.max(rnng_scores_zoom), np.max(lstm_scores_zoom)])
 
-            all_scores_zoom = np.concatenate(
-                [syn_scores_zoom[None, ...], glove_scores_zoom[None, ...], rnng_scores_zoom[None, ...], lstm_scores_zoom[None, ...], ])
+        all_scores_zoom = np.concatenate(
+            [syn_scores_zoom[None, ...], glove_scores_zoom[None, ...], rnng_scores_zoom[None, ...], lstm_scores_zoom[None, ...], ])
 
-            good_scores_zoom = all_scores_zoom>= 0.15
+        good_scores_zoom = all_scores_zoom>= 0.15
 
-            win_scores_zoom = np.argmax(all_scores_zoom, axis=0)
+        win_scores_zoom = np.argmax(all_scores_zoom, axis=0)
 
-            h1 = ax_zoom.plot(time_zoom, syn_scores_zoom)
-            # h2 = ax.plot(time, sem_scores)
-            h3 = ax_zoom.plot(time_zoom, glove_scores_zoom)
-            # h4 = ax.plot(time, w2v_scores)
-            h5 = ax_zoom.plot(time_zoom, rnng_scores_zoom)
-            h6 = ax_zoom.plot(time_zoom, lstm_scores_zoom)
-            h1[0].set_label('Syntax')
-            # h2[0].set_label('Simple Semantics')
-            h3[0].set_label('glove Semantics')
-            # h4[0].set_label('w2v Semantics')
-            h5[0].set_label('RNNG')
-            h6[0].set_label('LSTM')
-            ax_zoom.legend()
+        h1 = ax_zoom.plot(time_zoom, syn_scores_zoom)
+        # h2 = ax.plot(time, sem_scores)
+        h3 = ax_zoom.plot(time_zoom, glove_scores_zoom)
+        # h4 = ax.plot(time, w2v_scores)
+        h5 = ax_zoom.plot(time_zoom, rnng_scores_zoom)
+        h6 = ax_zoom.plot(time_zoom, lstm_scores_zoom)
+        h1[0].set_label('Syntax')
+        # h2[0].set_label('Simple Semantics')
+        h3[0].set_label('glove Semantics')
+        # h4[0].set_label('w2v Semantics')
+        h5[0].set_label('RNNG')
+        h6[0].set_label('LSTM')
+        ax_zoom.legend()
 
-            for i_time in range(all_scores_zoom.shape[-1]):
-                if good_scores_zoom[win_scores_zoom[i_time], i_time]:
-                    ax_zoom.scatter(time_zoom[i_time], all_scores_zoom[win_scores_zoom[i_time], i_time]+0.05, c=colors[win_scores_zoom[i_time]], linewidths=0.0)
+        for i_time in range(all_scores_zoom.shape[-1]):
+            if good_scores_zoom[win_scores_zoom[i_time], i_time]:
+                ax_zoom.scatter(time_zoom[i_time], all_scores_zoom[win_scores_zoom[i_time], i_time]+0.05, c=colors[win_scores_zoom[i_time]], linewidths=0.0)
 
-            ax_zoom.set_title(uni_reg[i_reg])
-            ax_zoom.set_xlim(0.0, args.tmax + 0.5)
-            ax_zoom.set_xticks(np.arange(0.0, args.tmax, 0.5))
+        ax_zoom.set_title(uni_reg[i_reg])
+        ax_zoom.set_xlim(0.0, args.tmax + 0.5)
+        ax_zoom.set_xticks(np.arange(0.0, args.tmax, 0.5))
 
         # ax.legend([h1, h2], ['Syntax', 'Semantics'])
     max_val = np.max(max_reg)
