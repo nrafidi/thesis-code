6bc6946bbf7fe38405a84d21da3f7f8beebfe031
nrafidi <nicole.rafidi@gmail.com>
2017-11-15 14:02:29 -0500

    more p value debugging - one-sided test

--- python/tests/test_pvals.py
+++ python/tests/test_pvals.py
@@ -85,10 +85,10 @@ def bhy_multiple_comparisons_procedure(uncorrected_pvalues, alpha=0.05):
 
 
 if __name__ == '__main__':
-    perm_accs = np.random.rand(100, 10, 2, 200)
+    perm_accs = np.random.rand(1000, 10, 2, 200)
     good_points = 0.999*np.ones((10, 2, 100))
 
-    for badness in [0.7, 0.8, 0.9]:
+    for badness in [0.7]:
         bad_points = badness*np.ones((10, 2, 100))
         true_accs = np.concatenate([good_points, bad_points], axis=2)
         print(true_accs.shape)
