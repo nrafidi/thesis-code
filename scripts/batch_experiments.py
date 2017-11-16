4a36dd926c2ed3bfa184ef92b6a66b57b19a7a14
nrafidi <nicole.rafidi@gmail.com>
2017-11-16 11:52:15 -0500

    reducing number of jobs

--- scripts/batch_experiments.py
+++ scripts/batch_experiments.py
@@ -108,5 +108,5 @@ if __name__ == '__main__':
         call(call_str, shell=True)
         job_id += 1
 
-        while int(check_output(JOB_Q_CHECK, shell=True)) >= 150:
+        while int(check_output(JOB_Q_CHECK, shell=True)) >= 100:
             time.sleep(30)
