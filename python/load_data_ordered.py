bb1f39b4d842946f30d38a69fc5ec03086fd852e
Nicole <nicole.rafidi@gmail.com>
2018-01-13 11:51:39 -0500

    too hacky must fix fix

--- python/load_data_ordered.py
+++ python/load_data_ordered.py
@@ -170,11 +170,13 @@ def is_noun_at_noun_count(ordered_sentence_usis, index_noun, is_raise=True):
     index = 0
     for (usi, annotation), is_match in has_part_of_speech(ordered_sentence_usis, noun_tags):
         if 'krns2' in usi:
+            print(usi)
             if (annotation['stimulus'] in WORD_PER_SEN['krns2']['active']['noun1'] or
                         annotation['stimulus'] in WORD_PER_SEN['krns2']['active']['noun2'] or
                         annotation['stimulus'] in WORD_PER_SEN['krns2']['passive']['noun1'] or
                         annotation['stimulus'] in WORD_PER_SEN['krns2']['passive']['noun2']):
                 if index == index_noun:
+                    print('yielding True')
                     yield True
                 else:
                     yield False
