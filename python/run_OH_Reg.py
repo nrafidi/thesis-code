865d726e0e06403bd326e1f63c21db82a786cec5
Nicole <nicole.rafidi@gmail.com>
2018-01-13 13:54:13 -0500

    fixing label index bug

--- python/run_OH_Reg.py
+++ python/run_OH_Reg.py
@@ -104,10 +104,12 @@ def run_sv_exp(experiment,
                                                                   noMag=False,
                                                                   sorted_inds=None)
 
-    l_set = np.unique(labels)
-    n_l = len(l_set)
-    l_index = {l_set[i]: i for i in xrange(n_l)}
-    l_ints = np.array([l_index[l] for l in labels])
+    l_set = np.unique(labels, axis=0)
+    n_l = l_set.shape[0]
+    num_labels = labels.shape[0]
+    l_ints = np.empty((num_labels,))
+    for i_l in range(num_labels):
+        l_ints[i_l] = np.where(l_set == labels[i_l, :])
 
     semantic_vectors = []
     for col in range(labels.shape[-1]):
