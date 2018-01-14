aa47bb0fe7407015c6ee4a4d1f25bd34f7ea3ed0
Nicole <nicole.rafidi@gmail.com>
2018-01-14 12:21:57 -0500

    fixed an incredibly stupid bug

--- python/plot_oh_var_partition.py
+++ python/plot_oh_var_partition.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     print('Post adj r2all = {}'.format(r2_all_adj[0]))
 
     r2_word = explained_variance_score(total_true_word, total_est_word, multioutput='raw_values')
-    r2_word_adj = np.ones((num_outputs,)) - (np.ones((num_outputs,)) - r2_all) * np.divide(float(num_samples - 1),
+    r2_word_adj = np.ones((num_outputs,)) - (np.ones((num_outputs,)) - r2_word) * np.divide(float(num_samples - 1),
                                                                                            float(num_samples - num_feats_all + 3))
     print('Pre adj r2word = {}'.format(r2_word[0]))
     print('Divisor = {}'.format(np.divide(float(num_samples - 1), float(num_samples - num_feats_all + 3))))
@@ -119,22 +119,24 @@ if __name__ == '__main__':
     r2_word_adj = r2_word_adj[sorted_inds, :]
 
     r2_plot = r2_all_adj - r2_word_adj
+    print('Max Unique Variance = {}'.format(np.max(r2_plot)))
+    print('Min Unique Variance = {}'.format(np.min(r2_plot)))
 
     uni_reg = np.unique(sorted_reg)
     yticks_sens = [sorted_reg.index(reg) for reg in uni_reg]
 
-    r2_all = np.reshape(r2_all, (306, -1))
-    r2_all = r2_all[sorted_inds, :]
-    fig, ax = plt.subplots()
-    h = ax.imshow(r2_all, interpolation='nearest', aspect='auto', vmin=0.0, vmax=0.6)
-    ax.set_yticks(yticks_sens)
-    ax.set_yticklabels(uni_reg)
-    ax.set_ylabel('Sensors')
-    ax.set_xticks(range(0, num_time, 250))
-    ax.set_xticklabels(time[::250])
-    ax.set_xlabel('Time')
-    ax.set_title('{} {} All\nAdj: {}'.format(exp, sen_type, adj))
-    plt.colorbar(h)
+    # r2_all = np.reshape(r2_all, (306, -1))
+    # r2_all = r2_all[sorted_inds, :]
+    # fig, ax = plt.subplots()
+    # h = ax.imshow(r2_all, interpolation='nearest', aspect='auto', vmin=0.0, vmax=0.6)
+    # ax.set_yticks(yticks_sens)
+    # ax.set_yticklabels(uni_reg)
+    # ax.set_ylabel('Sensors')
+    # ax.set_xticks(range(0, num_time, 250))
+    # ax.set_xticklabels(time[::250])
+    # ax.set_xlabel('Time')
+    # ax.set_title('{} {} All\nAdj: {}'.format(exp, sen_type, adj))
+    # plt.colorbar(h)
 
 
     fig, ax = plt.subplots()
