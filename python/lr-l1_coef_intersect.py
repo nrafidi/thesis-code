a6c898a3e69de839988d1999b9ad4a50ca27bed0
nrafidi <nicole.rafidi@gmail.com>
2018-01-31 16:17:13 -0500

    when using ovr you have to intersect over class

--- python/lr-l1_coef_intersect.py
+++ python/lr-l1_coef_intersect.py
@@ -49,6 +49,7 @@ def intersect_coef(exp,
                                                        mode='coef') + '.npz')
         coef = result['coef']
         coef_time = np.array(coef[win_time] != 0)
+        coef_time = np.all(coef_time, axis=0)
         coef_by_sub.append(coef_time[None, ...])
     intersection = np.all(np.concatenate(coef_by_sub, axis=0), axis=0)
     return intersection
