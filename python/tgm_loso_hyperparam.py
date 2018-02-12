6de1d1c12ce08262727cc196b974708f2b242dd6
Nicole <nicole.rafidi@gmail.com>
2018-02-12 10:48:13 -0500

    debugging some numpy index stuff

--- python/tgm_loso_hyperparam.py
+++ python/tgm_loso_hyperparam.py
@@ -51,11 +51,13 @@ if __name__ == '__main__':
                                                                      avgTest=args.avgTest)
 
             time_ind = np.where(time[win_starts] >= (max_time - time_adjust))
+            print(time_ind)
             frac_sub = np.diag(intersection).astype('float')/float(len(load_data.VALID_SUBS[args.experiment]))
             mean_acc = np.diag(np.mean(acc_all, axis=0))
 
             max_mean_acc = np.max(mean_acc[time_ind])
             argmax_mean_acc = np.argmax(mean_acc[time_ind])
+            print(argmax_mean_acc)
             arg_max_eos_win.append(time_ind[argmax_mean_acc])
 
             arg_max_tot_win.append(np.argmax(mean_acc))
