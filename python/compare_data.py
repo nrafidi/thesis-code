eeff4323b7eae38de53d36d2ee737170828679e8
Nicole <nicole.rafidi@gmail.com>
2018-02-27 15:30:55 -0500

    comparing data from different loading schemes

--- python/compare_data.py
+++ python/compare_data.py
@@ -40,13 +40,13 @@ if __name__ == '__main__':
                 h0 = ax[0].imshow(np.squeeze(data_new[i, :, :]), interpolation='nearest', aspect='auto')
                 text_to_write = ['Det', 'Noun1', 'Verb', 'Det', 'Noun2.']
                 max_line = 2.51 * 500
-                for i_v, v in enumerate(np.arange(0.5 * 500, max_line, 0.5 * 500)):
+                for i_v, v in enumerate(np.arange(0.0 * 500, max_line, 0.5 * 500)):
                     ax[0].axvline(x=v, color='k')
                     if i_v < len(text_to_write):
                         ax[0].text(v + 0.05 * 500, 15, text_to_write[i_v])
                 ax[0].set_title('New')
                 h1 = ax[1].imshow(np.squeeze(data_old[j, :, :]), interpolation='nearest', aspect='auto')
-                for i_v, v in enumerate(np.arange(0.5 * 500, max_line, 0.5 * 500)):
+                for i_v, v in enumerate(np.arange(0.0 * 500, max_line, 0.5 * 500)):
                     ax[1].axvline(x=v, color='k')
                     if i_v < len(text_to_write):
                         ax[1].text(v + 0.05 * 500, 15, text_to_write[i_v])
@@ -63,10 +63,22 @@ if __name__ == '__main__':
     ax[0].set_title('New')
     ax[1].imshow(total_mean_old_long, interpolation='nearest', aspect='auto')
     ax[1].set_title('Old')
+    for i_v, v in enumerate(np.arange(0.0 * 500, max_line, 0.5 * 500)):
+        ax[0].axvline(x=v, color='k')
+        if i_v < len(text_to_write):
+            ax[0].text(v + 0.05 * 500, 15, text_to_write[i_v])
+    for i_v, v in enumerate(np.arange(0.0 * 500, max_line, 0.5 * 500)):
+        ax[1].axvline(x=v, color='k')
+        if i_v < len(text_to_write):
+            ax[1].text(v + 0.05 * 500, 15, text_to_write[i_v])
     fig.suptitle('Mean over all Long Sentences')
 
     fig, ax = plt.subplots()
     ax.imshow(np.squeeze(np.abs(total_mean_new_long[:,:min_time] - total_mean_old_long[:,:min_time])),
               interpolation='nearest', aspect='auto')
+    for i_v, v in enumerate(np.arange(0.0 * 500, max_line, 0.5 * 500)):
+        ax.axvline(x=v, color='k')
+        if i_v < len(text_to_write):
+            ax.text(v + 0.05 * 500, 15, text_to_write[i_v])
     ax.set_title('Absolute Difference - Long Sentences')
     plt.show()
