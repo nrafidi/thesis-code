5d7c817384c613145191864e739760c370e5da13
Nicole <nicole.rafidi@gmail.com>
2018-03-26 10:39:06 -0400

    plot adjustments

--- python/diag_overlay_loso_acc.py
+++ python/diag_overlay_loso_acc.py
@@ -80,10 +80,10 @@ def intersect_accs(exp,
         eos_max_fold = np.array(eos_max_fold)
         eos_max_by_sub.append(eos_max_fold[None, :])
         acc = np.mean(fold_acc, axis=0)
-        if sub == 'B':
-            fig, ax = plt.subplots()
-            ax.plot(acc[:, 0])
-            ax.set_title('B')
+        # if sub == 'B':
+        #     fig, ax = plt.subplots()
+        #     ax.plot(acc[:, 0])
+        #     ax.set_title('B')
 
         time_by_sub.append(time[None, ...])
         win_starts_by_sub.append(win_starts[None, ...])
@@ -221,7 +221,7 @@ if __name__ == '__main__':
             for i_v, v in enumerate(np.arange(start_line[i_sen], max_line[i_sen], time_step)):
                 ax.axvline(x=v, color='k')
                 if i_v < len(text_to_write[i_sen]):
-                    plt.text(v + 0.15, 0.7 - i_sen*0.2, text_to_write[i_sen][i_v], color=color)
+                    plt.text(v + 0.15, 0.7 - i_sen*0.1, text_to_write[i_sen][i_v], color=color)
 
         ax.set_xticks(range(0, len(sen_time[-1]), time_step))
         label_time = sen_time[-1]
