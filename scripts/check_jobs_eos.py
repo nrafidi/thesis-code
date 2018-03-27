d20d095bb4d44c48a20f8c0393086a064793ec5d
Nicole <nicole.rafidi@gmail.com>
2018-03-27 10:48:00 -0400

    trying to see which jobs are completed

--- scripts/check_jobs_eos.py
+++ scripts/check_jobs_eos.py
@@ -50,8 +50,6 @@ if __name__ == '__main__':
         win_len = grid[12]
         sub = grid[13]
 
-        if job_id == 7892:
-            print(grid)
 
         job_str = batch_exp.JOB_NAME.format(exp=exp,
                                             sub=sub,
@@ -86,6 +84,7 @@ if __name__ == '__main__':
         if os.path.isfile(fname + '.npz'):
             successful_jobs += 1
             was_success = True
+            print(fname)
         else:
             was_success = False
 
