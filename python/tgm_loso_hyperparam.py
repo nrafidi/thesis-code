ac8d4f4bcfcfd2aba0b6d7016231dd52316078e3
nrafidi <nicole.rafidi@gmail.com>
2018-03-27 15:38:34 -0400

    plot adjustments

--- python/tgm_loso_hyperparam.py
+++ python/tgm_loso_hyperparam.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
 
     per_sub_mean_tot = np.concatenate(per_sub_mean_tot, axis=0)
 
-    fig, axs = plt.subplots(3, 2, figsize=(20, 10))
+    fig, axs = plt.subplots(3, 2, figsize=(10, 20))
     h00 = axs[0][0].imshow(frac_sub_tot, interpolation='nearest', vmin=0.5, vmax=1.0)
     axs[0][0].set_title('Fraction of Subjects > Chance\nGlobal Max Accuracy')
     fig.colorbar(h00, ax=axs[0][0])
