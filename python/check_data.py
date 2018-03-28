b904fa857f9f5795c63e58c329f4105d911b4b3d
Nicole <nicole.rafidi@gmail.com>
2018-03-28 19:57:24 -0400

    check krns2 data

--- python/check_data.py
+++ python/check_data.py
@@ -6,19 +6,22 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    data, labels, sen_ints, time, sensor_regions = load_data.load_sentence_data_v2(subject='B',
-                                                                                   align_to='verb',
-                                                                                   voice='active',
-                                                                                   experiment='krns2',
-                                                                                   proc=load_data.DEFAULT_PROC,
-                                                                                   num_instances=1,
-                                                                                   reps_filter=None,
-                                                                                   sensor_type=None,
-                                                                                   is_region_sorted=False,
-                                                                                   tmin=None,
-                                                                                   tmax=None)
-    data_to_plot = np.squeeze(np.mean(data, axis=0))
+    for sen_type in ['active', 'passive']:
+        for word in ['noun1', 'verb']:
+            data, labels, sen_ints, time, sensor_regions = load_data.load_sentence_data_v2(subject='B',
+                                                                                           align_to=word,
+                                                                                           voice=sen_type,
+                                                                                           experiment='krns2',
+                                                                                           proc=load_data.DEFAULT_PROC,
+                                                                                           num_instances=1,
+                                                                                           reps_filter=None,
+                                                                                           sensor_type=None,
+                                                                                           is_region_sorted=False,
+                                                                                           tmin=None,
+                                                                                           tmax=None)
+            data_to_plot = np.squeeze(np.mean(data, axis=0))
 
-    fig, ax = plt.subplots()
-    h = ax.imshow(data_to_plot, interpolation='nearest')
+            fig, ax = plt.subplots()
+            h = ax.imshow(data_to_plot, interpolation='nearest', aspect='auto')
+            ax.set_title('{} {}'.format(sen_type, word))
     plt.show()
