444ddc3c338c13766b5f744aa734cec16eda835e
Nicole <nicole.rafidi@gmail.com>
2018-04-18 15:37:32 -0400

    add noise ceiling

--- python/plot_bar_RSA.py
+++ python/plot_bar_RSA.py
@@ -216,8 +216,8 @@ if __name__ == '__main__':
                    color=colors[j_cond], label=COND_TITLE[cond_list[j_cond]])
 
         # add some text for labels, title and axes ticks
-        ax.fill_between(ind, mean_scores[:, 0, 0], mean_scores[:, 0, 0],
-                        facecolor=0.5, alpha=0.5, edgecolor='w')
+        ax.fill_between(ind, mean_scores[:, 0, 0], mean_scores[:, 0, 1],
+                        facecolor='0.5', alpha=0.5, edgecolor='w')
         ax.set_ylabel('Mean Correlation with Neural Data')
         ax.set_ylim([-0.1, 1.0])
         ax.set_title(MODEL_TITLES[i_model])
