f75330666a532763181e250e3c0d445ee16f904d
nrafidi <nicole.rafidi@gmail.com>
2018-04-20 10:39:19 -0400

    created nb_tgm_loso

--- python/models.py
+++ python/models.py
@@ -282,6 +282,160 @@ def get_pred_acc(preds, labels):
     return np.sum(label_est == labels)/len(labels)
 
 
+def nb_tgm_loso(data,
+                labels,
+                sen_ints,
+                sub_rs,
+                win_starts,
+                win_len,
+                feature_select=False,
+                doZscore=False,
+                doAvg=False,
+                ddof=1):
+
+    labels = np.array(labels)
+    n_tot = data.shape[0]
+    n_time = data.shape[2]
+
+    l_set = np.unique(labels)
+    n_l = len(l_set)
+    l_index = {l_set[i]: i for i in xrange(n_l)}
+    l_ints = np.array([l_index[l] for l in labels])
+    in_l = [l_ints == i for i in xrange(n_l)]
+
+    test_windows = [np.array([i >= w_s and i < w_s + win_len for i in xrange(n_time)]) for w_s in win_starts]
+    n_w = len(test_windows)
+
+    uni_sen_ints = np.unique(sen_ints)
+
+    preds = np.empty((kf.get_n_splits(), n_w, n_w), dtype=np.object)
+    cv_membership = np.empty((kf.get_n_splits(),), dtype=np.object)
+    feature_masks = np.empty((kf.get_n_splits(), n_w), dtype=np.object)
+    num_feat_selected = np.empty((kf.get_n_splits(), n_w))
+    # Top-level CV
+    i_top_split = 0
+    for lint in uni_sen_ints:
+        in_test = sen_ints == lint
+        in_train = np.logical_not(in_test)
+        print(i_top_split)
+        sub_kf = KFold(n_splits=len(in_train), shuffle=True, random_state=sub_rs)
+        cv_membership[i_top_split] = in_test
+        # Iterate over full time grid
+        for wi in xrange(n_w):
+            train_time = test_windows[wi]
+            train_data = data[in_train, :, :]
+            train_data = train_data[:, :, train_time]
+            print(train_data.shape)
+            if doAvg:
+                train_data = np.mean(train_data, axis=2)
+            if doZscore:
+                mu_full_all = np.mean(train_data, axis=0)
+                std_full_all = np.std(train_data, axis=0, ddof=ddof)
+                train_data_z = train_data - mu_full_all[None, ...]
+                train_data_z /= std_full_all[None, ...]
+            else:
+                train_data_z = train_data
+
+            train_l_ints = l_ints[in_train]
+            train_in_l = [in_l[li][in_train] for li in xrange(n_l)]
+
+            A_top, B_top, mu_full_top = gnb_model(train_data_z, train_in_l, ddof)
+
+            if feature_select:
+                num_feat_opts = flatten_list(NUM_FEAT_OPTIONS)
+                num_feat_accs = np.empty((len(num_feat_opts), sub_kf.get_n_splits()))
+                # Inner CV for Feature Selection
+                meow = np.reshape(train_data, (train_data.shape[0], -1))
+                i_split = 0
+                for in_sub_train, in_sub_test in sub_kf.split(meow, train_l_ints):
+                    sub_train_data = train_data[in_sub_train, ...]
+                    sub_test_data = train_data[in_sub_test, ...]
+                    sub_test_ints = train_l_ints[in_sub_test]
+                    sub_train_in_l = [train_in_l[li][in_sub_train] for li in xrange(n_l)]
+
+                    if doZscore:
+                        mu_full_all = np.mean(sub_train_data, axis=0)
+                        std_full_all = np.std(sub_train_data, axis=0, ddof=ddof)
+                        sub_train_data -= mu_full_all[None, ...]
+                        sub_train_data /= std_full_all[None, ...]
+                        sub_test_data -= mu_full_all[None, ...]
+                        sub_test_data /= std_full_all[None, ...]
+                    # print('{}/{}'.format(wi, n_w))
+                    # print(sub_train_in_l)
+                    A, B, mu_full = gnb_model(sub_train_data, sub_train_in_l, ddof)
+                    mu_diff = reduce(lambda accum, lis: accum + np.abs(mu_full[lis[0]] - mu_full[lis[1]]),
+                                     ((li1, li2) for li1 in xrange(n_l) for li2 in xrange(li1 + 1, n_l)),
+                                     np.zeros(mu_full[0].shape))
+                    sorted_indices = np.argsort(-mu_diff, axis=None)
+
+                    for i_feat_num, feat_num in enumerate(num_feat_opts):
+                        if feat_num > len(sorted_indices):
+                            break
+                        mask = np.zeros(mu_diff.shape, dtype=np.bool)
+                        mask[np.unravel_index(sorted_indices[:feat_num], mu_diff.shape)] = True
+                        A_mask = np.multiply(mask[None, ...], A)
+                        if doAvg:
+                            B_mask = np.sum(np.multiply(B, mask[None, ...]), axis=1)
+                        else:
+                            B_mask = np.sum(np.multiply(B, mask[None, ...]), axis=(1, 2))
+                        if doAvg:
+                            pred_mask = np.sum(np.multiply(sub_test_data[:, None, ...], A_mask[None, ...]),
+                                               axis=2) - B_mask
+                        else:
+                            pred_mask = np.sum(np.multiply(sub_test_data[:, None, ...], A_mask[None, ...]),
+                                               axis=(2, 3)) - B_mask
+                        num_feat_accs[i_feat_num, i_split] = get_pred_acc(pred_mask, sub_test_ints)
+                    i_split += 1
+
+                best_feat_ind = np.argmax(np.mean(num_feat_accs, axis=1))
+                feat_num = num_feat_opts[best_feat_ind]
+                mu_diff = reduce(lambda accum, lis: accum + np.abs(mu_full_top[lis[0]] - mu_full_top[lis[1]]),
+                                 ((li1, li2) for li1 in xrange(n_l) for li2 in xrange(li1 + 1, n_l)),
+                                 np.zeros(mu_full_top[0].shape))
+                sorted_indices = np.argsort(-mu_diff, axis=None)
+                mask = np.zeros(mu_diff.shape, dtype=np.bool)
+                mask[np.unravel_index(sorted_indices[:feat_num], mu_diff.shape)] = True
+                feature_masks[i_top_split, wi] = mask
+                num_feat_selected[i_top_split, wi] = feat_num
+            else:
+                mask = np.ones(A_top.shape[1:], dtype=np.bool)
+
+            A_top = np.multiply(mask[None, ...], A_top)
+            if doAvg:
+                B_top = np.sum(np.multiply(B_top, mask[None, ...]), axis=1)
+            else:
+                B_top = np.sum(np.multiply(B_top, mask[None, ...]), axis=(1, 2))
+
+            for wj in xrange(n_w):
+                test_time = test_windows[wj]
+                test_data = data[in_test, :, :]
+                test_data = test_data[:, :, test_time]
+                if doAvg:
+                    test_data = np.mean(test_data, axis=2)
+                if doZscore:
+                    mu_full_all = np.mean(train_data, axis=0)
+                    std_full_all = np.std(train_data, axis=0, ddof=ddof)
+                    test_data -= mu_full_all[None, ...]
+                    test_data /= std_full_all[None, ...]
+                print(test_data.shape)
+                print('A')
+                print(A_top.shape)
+                print('B')
+                print(B_top.shape)
+                meow = np.multiply(test_data[:, None, ...], A_top[None, ...])
+                print('meow')
+                print(meow.shape)
+                if doAvg:
+                    pred_top = np.sum(np.multiply(test_data[:, None, ...], A_top[None, ...]),
+                                       axis=2) - B_top
+                else:
+                    pred_top = np.sum(np.multiply(test_data[:, None, ...], A_top[None, ...]),
+                                      axis=(2, 3)) - B_top
+                preds[i_top_split, wi, wj] = pred_top
+        i_top_split += 1
+    return preds, l_ints, cv_membership, feature_masks, num_feat_selected
+
+
 def nb_tgm(data,
            labels,
            kf,
