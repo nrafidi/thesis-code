4ff07456f756aab08a5b0626e97afb49949e5a5b
Nicole <nicole.rafidi@gmail.com>
2018-04-23 11:09:00 -0400

    forgot a space!

--- scripts/batch_experiments_alg_time.py
+++ scripts/batch_experiments_alg_time.py
@@ -12,7 +12,7 @@ OUT_FILE = JOB_DIR + JOB_NAME + '.o'
 
 if __name__ == '__main__':
 
-    qsub_call = 'qsub  -q default -N ' + JOB_NAME + '-l walltime=200:00:00,mem=2GB ' \
+    qsub_call = 'qsub  -q default -N ' + JOB_NAME + ' -l walltime=200:00:00,mem=2GB ' \
                 '-e {errfile} -o {outfile} submit_experiment_alg.sh'
 
     call(qsub_call, shell=True)
