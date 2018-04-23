c347416ff5c87acbf22ed41c0df04462d53b267c
Nicole <nicole.rafidi@gmail.com>
2018-04-23 11:13:30 -0400

    small bug fixes

--- scripts/batch_experiments_alg_time.py
+++ scripts/batch_experiments_alg_time.py
@@ -13,7 +13,7 @@ OUT_FILE = JOB_DIR + JOB_NAME + '.o'
 if __name__ == '__main__':
 
     qsub_call = 'qsub  -q default -N ' + JOB_NAME + ' -l walltime=200:00:00,mem=2GB ' \
-                '-e {errfile} -o {outfile} submit_experiment_alg.sh'
+                '-e {errfile} -o {outfile} submit_experiment_alg_time.sh'
 
     call(qsub_call.format(errfile=ERR_FILE,
                           outfile=OUT_FILE), shell=True)
