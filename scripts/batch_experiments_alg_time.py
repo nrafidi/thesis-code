ea67a168f11458db577bc80a9f5712edcf9df6d8
Nicole <nicole.rafidi@gmail.com>
2018-04-23 11:10:35 -0400

    forgot to include error files

--- scripts/batch_experiments_alg_time.py
+++ scripts/batch_experiments_alg_time.py
@@ -15,4 +15,5 @@ if __name__ == '__main__':
     qsub_call = 'qsub  -q default -N ' + JOB_NAME + ' -l walltime=200:00:00,mem=2GB ' \
                 '-e {errfile} -o {outfile} submit_experiment_alg.sh'
 
-    call(qsub_call, shell=True)
+    call(qsub_call.format(errfile=ERR_FILE,
+                          outfile=OUT_FILE), shell=True)
