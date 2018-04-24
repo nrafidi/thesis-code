f8eeb53c53ec52c01c0a02323b154c74a3c45236
Nicole <nicole.rafidi@gmail.com>
2018-04-23 23:20:18 -0400

    double check l2 results

--- python/run_alg_comp.py
+++ python/run_alg_comp.py
@@ -16,7 +16,7 @@ NUM_REPS = {'krns2': 15, 'PassAct2': 10, 'PassAct3': 10}
 VALID_SUBS = {'krns2': ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
               'PassAct2': ['A', 'B', 'C'],
               'PassAct3': ['A', 'B', 'C', 'E', 'F', 'G', 'J', 'K', 'L', 'N', 'O', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z']}
-VALID_ALGS = ['lr-l2', 'lr-l1', 'svm-l2', 'svm-l1', 'gnb']
+VALID_ALGS = ['lr-l1', 'lr-l2', 'svm-l2', 'svm-l1', 'gnb']
 VALID_WORDS = ['verb', 'voice']
 
 
