75f99ec33402716411c7ff78c7f11260665b4a93
Nicole <nicole.rafidi@gmail.com>
2018-05-07 10:16:15 -0400

    plot only relevant hyperparams

--- python/tgm_loso_hyperparam.py
+++ python/tgm_loso_hyperparam.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     win_lens = [12, 25, 50, 100, 150]
-    num_insts = [2, 5, 10]
+    num_insts = [1, 2, 5, 10]
 
     perc = args.percentile
 
@@ -46,8 +46,8 @@ if __name__ == '__main__':
                           axes_pad=0.7, cbar_mode='single', cbar_location='right',
                           cbar_pad=0.5)
     i_avg_combo = 0
-    for avgTime in ['T', 'F']:
-        for avgTest in ['T', 'F']:
+    for avgTime in ['F']:
+        for avgTest in ['T']:
             per_sub_perc_tot = []
             if avgTime == 'T':
                 avg_time_str = 'Time Average'
@@ -308,37 +308,37 @@ if __name__ == '__main__':
             optimal = np.unravel_index(np.argmax(all_combined), all_combined.shape)
 
             # fig, ax = plt.subplots()
-            ax = avg_combo_grid[i_avg_combo]
-            avg_im = ax.imshow(all_combined, interpolation='nearest', vmin=-7.0, vmax=7.0)
-
-            ax.set_title('{avgTime}\n{avgTest}'.format(
-                avgTime=avg_time_str,
-                avgTest=avg_test_str),
-                         fontsize=14)
-            ax.set_xticks(range(len(num_insts)))
-            ax.set_xticklabels(num_insts)
-            ax.set_yticks(range(len(win_lens)))
-            ax.set_yticklabels(np.array(win_lens).astype('float') * 2)
-            if i_avg_combo > 1:
-                ax.set_xlabel('Number of Instances')
-            if i_avg_combo == 0 or i_avg_combo == 2:
-                ax.set_ylabel('Window Length (ms)')
-            ax.text(-0.15, 1.05, string.ascii_uppercase[i_avg_combo], transform=ax.transAxes,
-                    size=20, weight='bold')
-            i_avg_combo += 1
+            # ax = avg_combo_grid[i_avg_combo]
+            # avg_im = ax.imshow(all_combined, interpolation='nearest', vmin=-7.0, vmax=7.0)
+            #
+            # ax.set_title('{avgTime}\n{avgTest}'.format(
+            #     avgTime=avg_time_str,
+            #     avgTest=avg_test_str),
+            #              fontsize=14)
+            # ax.set_xticks(range(len(num_insts)))
+            # ax.set_xticklabels(num_insts)
+            # ax.set_yticks(range(len(win_lens)))
+            # ax.set_yticklabels(np.array(win_lens).astype('float') * 2)
+            # if i_avg_combo > 1:
+            #     ax.set_xlabel('Number of Instances')
+            # if i_avg_combo == 0 or i_avg_combo == 2:
+            #     ax.set_ylabel('Window Length (ms)')
+            # ax.text(-0.15, 1.05, string.ascii_uppercase[i_avg_combo], transform=ax.transAxes,
+            #         size=20, weight='bold')
+            # i_avg_combo += 1
 
 
             print(
             'Optimal window size: {win}\nOptimal number of instances: {ni}\nScore: {score}'.format(win=win_lens[optimal[0]],
                                                                                                 ni=num_insts[optimal[1]],
                                                                                                     score=np.max(all_combined)))
-    cbar = avg_combo_grid.cbar_axes[0].colorbar(avg_im)
-    avg_combo_fig.suptitle('Total Combined Scores',
-        fontsize=18)
-    avg_combo_fig.savefig(fig_fname.format(
-        exp=args.experiment, sen_type='both', word='all', alg=args.alg, avgTime='TF', avgTest='TF',
-        perc=perc, fig_type='total-comb-max-score-comp-avg'
-    ), bbox_inches='tight')
+    # cbar = avg_combo_grid.cbar_axes[0].colorbar(avg_im)
+    # avg_combo_fig.suptitle('Total Combined Scores',
+    #     fontsize=18)
+    # avg_combo_fig.savefig(fig_fname.format(
+    #     exp=args.experiment, sen_type='both', word='all', alg=args.alg, avgTime='TF', avgTest='TF',
+    #     perc=perc, fig_type='total-comb-max-score-comp-avg'
+    # ), bbox_inches='tight')
 
     plt.show()
     #
