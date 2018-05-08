f1ce93b5801a751e0d22565ae9a2de47d8f04b16
Nicole <nicole.rafidi@gmail.com>
2018-05-08 11:55:03 -0400

    noun2 passive is plotting weird

--- python/tgm_loso_acc.py
+++ python/tgm_loso_acc.py
@@ -237,7 +237,10 @@ if __name__ == '__main__':
                 sen_type=PLOT_TITLE_SEN[sen_type],
                 word=PLOT_TITLE_WORD[word]), fontsize=14)
 
-            ax.set_xticks(np.arange(0.0, float(num_time), float(time_step)) - time_adjust)
+            if word != 'noun2' and sen_type != 'passive':
+                ax.set_xticks(np.arange(0.0, float(num_time), float(time_step)) - time_adjust)
+            else:
+                print(ax.get_xticks())
 
             min_time = 0.0
             max_time = 0.5 * len(time_win) / time_step
