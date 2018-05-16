1d086df362204b9efcf986ca09a3112e99c71d5c
nrafidi <nicole.rafidi@gmail.com>
2018-05-16 14:02:15 -0400

    sample plots for defense

--- python/plot_example.py
+++ python/plot_example.py
@@ -7,10 +7,17 @@ import numpy as np
 if __name__ == '__main__':
     x = np.linspace(0.0, 20.0, 1000)
 
-    sig = np.sin(x) + np.cos(3*x) + np.sin(10*x)
+    sig = np.sin(x) + np.sin(3*x) + np.sin(10*x)
+
+    win_starts = np.arange(0.0, 20.0, 2.0)
+    time_wins = [np.logical_and(x >= win, x <= (win + 2.0)) for win in win_starts]
+
+    avg_sig = np.array([np.mean(sig[tm]) for tm in time_wins])
+
 
     fig, ax = plt.subplots()
-    ax.plot(x, sig)
+    ax.plot(sig)
+    ax.plot(avg_sig)
 
-    ax.plot(x[::10], sig[::10])
+
     plt.show()
