83e4c7f31a1575504ad8fc863b52c333c8107885
Nicole <nicole.rafidi@gmail.com>
2018-05-17 13:51:22 -0400

    launch extra jobs to be done faster

--- scripts/batch_experiments.py
+++ scripts/batch_experiments.py
@@ -121,5 +121,5 @@ if __name__ == '__main__':
             call(call_str, shell=True)
         job_id += 1
 
-        while int(check_output(JOB_Q_CHECK, shell=True)) >= 100:
+        while int(check_output(JOB_Q_CHECK, shell=True)) >= 200:
             time.sleep(30)
