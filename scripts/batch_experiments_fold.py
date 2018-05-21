bed6eb6817fd7f3100020735ceb74faae6d5b6d1
Nicole <nicole.rafidi@gmail.com>
2018-05-21 10:16:52 -0400

    code for parallelizing over folds

---
+++ scripts/batch_experiments_fold.py
@@ -0,0 +1,137 @@
+import itertools
+import os.path
+from subprocess import call, check_output
+import time
+
+TOP_DIR = '/share/volume0/nrafidi/{exp}_TGM_LOSO/'
+SAVE_DIR = '{top_dir}/{sub}/'
+SAVE_FILE = '{dir}TGM-LOSO_{sub}_{sen_type}_{word}_win{win_len}_ov{ov}_pr{perm}_' \
+            'alg{alg}_adj-{adj}_avgTime{avgTm}_avgTest{avgTst}_ni{inst}_' \
+            'nr{rep}_rsPerm{rsP}_{mode}'
+
+FOLDS = range(16) #, 'coef']
+EXPERIMENTS = ['krns2'] #, 'PassAct3']
+SUBJECTS = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] #['A', 'B', 'C', 'E', 'F', 'G', 'J', 'K', 'L', 'N', 'O', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z']
+SEN_TYPES = ['active', 'passive'] #, 'active']
+WORDS = ['noun1', 'verb', 'noun2']
+WIN_LENS = [12, 25, 50, 100]
+OVERLAPS = [12]
+IS_PERMS = [False]  # True
+ALGS = ['lr-l2']  # GNB
+ADJS = ['zscore']
+DO_TIME_AVGS = [False]
+DO_TEST_AVGS = [True]#, True]  # True
+NUM_INSTANCESS = [1, 2, 5, 10]
+REPS_TO_USES = [None]  # 10
+RANDOM_STATES = [1]
+
+JOB_NAME = '{exp}-{sub}-{sen}-{word}-{id}'
+JOB_DIR = '/share/volume0/nrafidi/{exp}_jobFiles/'
+ERR_FILE = '{dir}{job_name}.e'
+OUT_FILE = '{dir}{job_name}.o'
+
+JOB_Q_CHECK = 'expr $(qselect -q default -u nrafidi | xargs qstat -u nrafidi | wc -l) - 5'
+# JOB_Q_CHECK = 'expr $(qselect -q pool2 -u nrafidi | xargs qstat -u nrafidi | wc -l) - 5'
+
+def bool_to_str(bool_var):
+    if bool_var:
+        return 'T'
+    else:
+        return 'F'
+
+if __name__ == '__main__':
+
+    # -q default -N {job_name} -l walltime=72:00:00,mem=2GB -v ' \
+    qsub_call = 'qsub -q default -N {job_name} -l walltime=168:00:00,mem=8GB -v ' \
+                'experiment={exp},subject={sub},sen_type={sen},word={word},win_len={win_len},overlap={overlap},' \
+                'isPerm={perm},adj={adj},alg={alg},doTimeAvg={tm_avg},fold={fold},' \
+                'doTestAvg={tst_avg},num_instances={inst},reps_to_use={rep},perm_random_state={rs},force=False, ' \
+                '-e {errfile} -o {outfile} submit_experiment_fold.sh'
+
+    param_grid = itertools.product(FOLDS,
+                                   EXPERIMENTS,
+                                   OVERLAPS,
+                                   IS_PERMS,
+                                   ALGS,
+                                   ADJS,
+                                   DO_TIME_AVGS,
+                                   DO_TEST_AVGS,
+                                   NUM_INSTANCESS,
+                                   REPS_TO_USES,
+                                   RANDOM_STATES,
+                                   WIN_LENS,
+                                   SEN_TYPES,
+                                   WORDS,
+                                   SUBJECTS)
+    job_id = 0
+    for grid in param_grid:
+        fold = grid[0]
+        exp = grid[1]
+        overlap = grid[2]
+        isPerm = grid[3]
+        alg = grid[4]
+        adj = grid[5]
+        tm_avg = grid[6]
+        tst_avg = grid[7]
+        ni = grid[8]
+        reps = grid[9]
+        rs = grid[10]
+        win_len = grid[11]
+        sen = grid[12]
+        word = grid[13]
+        sub = grid[14]
+
+        job_str = JOB_NAME.format(exp=exp,
+                                  sub=sub,
+                                  sen=sen,
+                                  word=word,
+                                  id=job_id)
+
+        dir_str = JOB_DIR.format(exp=exp)
+        if not os.path.exists(dir_str):
+            os.mkdir(dir_str)
+
+        err_str = ERR_FILE.format(dir=dir_str, job_name=job_str)
+        out_str = OUT_FILE.format(dir=dir_str, job_name=job_str)
+
+        call_str = qsub_call.format(job_name=job_str,
+                                    exp=exp,
+                                    sub=sub,
+                                    sen=sen,
+                                    word=word,
+                                    win_len=win_len,
+                                    overlap=overlap,
+                                    perm=isPerm,
+                                    adj=adj,
+                                    alg=alg,
+                                    tm_avg=tm_avg,
+                                    tst_avg=tst_avg,
+                                    fold=fold,
+                                    inst=ni,
+                                    rep=reps,
+                                    rs=rs,
+                                    errfile=err_str,
+                                    outfile=out_str)
+        top_dir = TOP_DIR.format(exp=exp)
+        save_dir = SAVE_DIR.format(top_dir=top_dir, sub=sub)
+        old_job = SAVE_FILE.format(dir=save_dir,
+                                 sub=sub,
+                                 sen_type=sen,
+                                 word=word,
+                                 win_len=win_len,
+                                 ov=overlap,
+                                 perm=bool_to_str(isPerm),
+                                 alg=alg,
+                                 adj=adj,
+                                 avgTm=bool_to_str(tm_avg),
+                                 avgTst=bool_to_str(tst_avg),
+                                 inst=ni,
+                                 rep=reps,
+                                 rsP=rs,
+                                 mode='acc')
+        if not os.path.isfile(old_job + '.npz'):
+            call(call_str, shell=True)
+        job_id += 1
+
+        while int(check_output(JOB_Q_CHECK, shell=True)) >= 200:
+            time.sleep(30)
---
+++ scripts/check_jobs_fold.py
@@ -0,0 +1,130 @@
+import itertools
+import os.path
+import batch_experiments as batch_exp
+
+TOP_DIR = '/share/volume0/nrafidi/{exp}_TGM_LOSO/'
+SAVE_DIR = '{top_dir}/{sub}/'
+SAVE_FILE = '{dir}TGM-LOSO_{sub}_{sen_type}_{word}_win{win_len}_ov{ov}_pr{perm}_' \
+            'alg{alg}_adj-{adj}_avgTime{avgTm}_avgTest{avgTst}_ni{inst}_' \
+            'nr{rep}_rsPerm{rsP}_{mode}'
+
+NEW_SAVE_FILE = '{dir}TGM-LOSO_{sub}_{sen_type}_{word}_win{win_len}_ov{ov}_pr{perm}_' \
+            'alg{alg}_adj-{adj}_avgTime{avgTm}_avgTest{avgTst}_ni{inst}_' \
+            'nr{rep}_rsPerm{rsP}_{mode}'
+
+
+def bool_to_str(bool_var):
+    if bool_var:
+        return 'T'
+    else:
+        return 'F'
+
+
+if __name__ == '__main__':
+    param_grid = itertools.product(batch_exp.FOLDS,
+                                   batch_exp.EXPERIMENTS,
+                                   batch_exp.OVERLAPS,
+                                   batch_exp.IS_PERMS,
+                                   batch_exp.ALGS,
+                                   batch_exp.ADJS,
+                                   batch_exp.DO_TIME_AVGS,
+                                   batch_exp.DO_TEST_AVGS,
+                                   batch_exp.NUM_INSTANCESS,
+                                   batch_exp.REPS_TO_USES,
+                                   batch_exp.RANDOM_STATES,
+                                   batch_exp.WIN_LENS,
+                                   batch_exp.SEN_TYPES,
+                                   batch_exp.WORDS,
+                                   batch_exp.SUBJECTS)
+    job_id = 0
+    successful_jobs = 0
+    skipped_jobs = 0
+    for grid in param_grid:
+        fold = grid[0]
+        exp = grid[1]
+        overlap = grid[2]
+        isPerm = grid[3]
+        alg = grid[4]
+        adj = grid[5]
+        tm_avg = grid[6]
+        tst_avg = grid[7]
+        ni = grid[8]
+        reps = grid[9]
+        rs = grid[10]
+        win_len = grid[11]
+        sen = grid[12]
+        word = grid[13]
+        sub = grid[14]
+
+        job_str = batch_exp.JOB_NAME.format(exp=exp,
+                                            sub=sub,
+                                            sen=sen,
+                                            word=word,
+                                            id=job_id)
+
+        dir_str = batch_exp.JOB_DIR.format(exp=exp)
+
+        err_str = batch_exp.ERR_FILE.format(dir=dir_str, job_name=job_str)
+        out_str = batch_exp.OUT_FILE.format(dir=dir_str, job_name=job_str)
+
+        top_dir = TOP_DIR.format(exp=exp)
+        if not os.path.exists(top_dir):
+            os.mkdir(top_dir)
+        save_dir = SAVE_DIR.format(top_dir=top_dir, sub=sub)
+        old_job = SAVE_FILE.format(dir=save_dir,
+                                 sub=sub,
+                                 sen_type=sen,
+                                 word=word,
+                                 win_len=win_len,
+                                 ov=overlap,
+                                 perm=bool_to_str(isPerm),
+                                 alg=alg,
+                                 adj=adj,
+                                 avgTm=bool_to_str(tm_avg),
+                                 avgTst=bool_to_str(tst_avg),
+                                 inst=ni,
+                                 rep=reps,
+                                 rsP=rs,
+                                 mode='acc')
+        fname = NEW_SAVE_FILE.format(dir=save_dir,
+                                 sub=sub,
+                                 sen_type=sen,
+                                 word=word,
+                                 win_len=win_len,
+                                 ov=overlap,
+                                 perm=bool_to_str(isPerm),
+                                 alg=alg,
+                                 adj=adj,
+                                 avgTm=bool_to_str(tm_avg),
+                                 avgTst=bool_to_str(tst_avg),
+                                 inst=ni,
+                                 rep=reps,
+                                 rsP=rs,
+                                 fold=fold)
+        if os.path.isfile(old_job + '.npz'):
+            skipped_jobs += 1
+            was_success = True
+        elif os.path.isfile(fname + '.npz'):
+            successful_jobs += 1
+            was_success = True
+        else:
+            was_success = False
+
+        if not was_success:
+            with open(out_str, 'r') as fid:
+                out_info = fid.read()
+            if 'Experiment parameters not valid.' in out_info:
+                skipped_jobs += 1
+            elif os.stat(err_str).st_size != 0:
+                with open(err_str, 'r') as fid:
+                    err_file = fid.read()
+                    if not err_file.endswith('warnings.warn(_use_error_msg)\n') and not ('Killed' in err_file):
+                        if 'MemoryError' in err_file:
+                            print('Job {} Failed'.format(job_str))
+                        else:
+                            print(err_file)
+                            print(grid)
+
+        job_id += 1
+
+    print('{}/{} jobs succeeded'.format(successful_jobs, job_id - skipped_jobs))
