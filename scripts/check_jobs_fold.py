e452e7647e122d26cf6fde9e44d7ceb9dd1ac3b1
Nicole <nicole.rafidi@gmail.com>
2018-05-21 10:19:01 -0400

    quick bug fix

--- scripts/check_jobs_fold.py
+++ scripts/check_jobs_fold.py
@@ -1,6 +1,6 @@
 import itertools
 import os.path
-import batch_experiments as batch_exp
+import batch_experiments_fold as batch_exp
 
 TOP_DIR = '/share/volume0/nrafidi/{exp}_TGM_LOSO/'
 SAVE_DIR = '{top_dir}/{sub}/'
