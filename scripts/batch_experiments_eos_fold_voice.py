cc042372b91cc93a340cf7c820b72cd6e7a48d94
nrafidi <nicole.rafidi@gmail.com>
2018-05-23 10:51:58 -0400

    EOS to run on single voice

---
+++ scripts/batch_experiments_eos_fold_voice.py
@@ -0,0 +1,135 @@
+import itertools
+import os.path
+from subprocess import call, check_output
+import time
+
+TOP_DIR = '/share/volume0/nrafidi/{exp}_TGM_LOSO_EOS/'
+SAVE_DIR = '{top_dir}/{sub}/'
+SAVE_FILE = '{dir}TGM-LOSO-EOS_{sub}_{sen_type}_{word}_win{win_len}_ov{ov}_pr{perm}_' \
+            'alg{alg}_adj-{adj}_avgTime{avgTm}_avgTest{avgTst}_ni{inst}_' \
+            'rsPerm{rsP}_{mode}'
+
+
+def bool_to_str(bool_var):
+    if bool_var:
+        return 'T'
+    else:
+        return 'F'
+
+
+FOLDS = range(16)
+EXPERIMENTS = ['krns2']#, 'PassAct3']
+SUBJECTS = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] #['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N']
+SEN_TYPES = ['active', 'passive']
+WORDS = ['verb', 'agent', 'patient', 'propid']
+WIN_LENS = [12, 25, 50, 100]
+OVERLAPS = [12]
+IS_PERMS = [False]
+ALGS = ['lr-l2']
+ADJS = ['zscore']
+DO_TME_AVGS = [False]
+DO_TST_AVGS = [True]
+NUM_INSTANCESS = [2, 5, 10]
+RANDOM_STATES = [1]
+
+JOB_NAME = '{exp}-{sub}-{sen}-{word}-{id}'
+JOB_DIR = '/share/volume0/nrafidi/{exp}_jobFiles/'
+ERR_FILE = '{dir}{job_name}.e'
+OUT_FILE = '{dir}{job_name}.o'
+
+JOB_Q_CHECK = 'expr $(qselect -q default -u nrafidi | xargs qstat -u nrafidi | wc -l) - 5'
+
+
+if __name__ == '__main__':
+
+    qsub_call = 'qsub  -q default -N {job_name} -l walltime=192:00:00,mem=8GB -v ' \
+                'experiment={exp},subject={sub},sen_type={sen},word={word},win_len={win_len},overlap={overlap},' \
+                'isPerm={perm},adj={adj},alg={alg},doTimeAvg={tm_avg},fold={fold},' \
+                'doTestAvg={tst_avg},num_instances={inst},perm_random_state={rs},force=False, ' \
+                '-e {errfile} -o {outfile} submit_experiment_eos_fold.sh'
+
+    param_grid = itertools.product(FOLDS,
+                                   EXPERIMENTS,
+                                   OVERLAPS,
+                                   IS_PERMS,
+                                   ALGS,
+                                   ADJS,
+                                   DO_TME_AVGS,
+                                   DO_TST_AVGS,
+                                   NUM_INSTANCESS,
+                                   RANDOM_STATES,
+                                   WIN_LENS,
+                                   SEN_TYPES,
+                                   WORDS,
+                                   SUBJECTS)
+    job_id = 0
+    for grid in param_grid:
+        fold = grid[0]
+        exp = grid[1]
+        overlap = grid[2]
+        isPerm = grid[3]
+        alg = grid[4]
+        adj = grid[5]
+        tm_avg = grid[6]
+        tst_avg = grid[7]
+        ni = grid[8]
+        rs = grid[9]
+        win_len = grid[10]
+        sen = grid[11]
+        word = grid[12]
+        sub = grid[13]
+
+        job_str = JOB_NAME.format(exp=exp,
+                                  sub=sub,
+                                  sen=sen,
+                                  word=word,
+                                  id=job_id)
+
+        dir_str = JOB_DIR.format(exp=exp)
+        if not os.path.exists(dir_str):
+            os.mkdir(dir_str)
+
+        err_str = ERR_FILE.format(dir=dir_str, job_name=job_str)
+        out_str = OUT_FILE.format(dir=dir_str, job_name=job_str)
+
+        call_str = qsub_call.format(job_name=job_str,
+                                    exp=exp,
+                                    sub=sub,
+                                    sen=sen,
+                                    word=word,
+                                    win_len=win_len,
+                                    overlap=overlap,
+                                    perm=isPerm,
+                                    adj=adj,
+                                    alg=alg,
+                                    tm_avg=tm_avg,
+                                    tst_avg=tst_avg,
+                                    fold=fold,
+                                    inst=ni,
+                                    rs=rs,
+                                    errfile=err_str,
+                                    outfile=out_str)
+
+        top_dir = TOP_DIR.format(exp=exp)
+        save_dir = SAVE_DIR.format(top_dir=top_dir, sub=sub)
+        fname = SAVE_FILE.format(dir=save_dir,
+                                 sub=sub,
+                                 sen_type=sen,
+                                 word=word,
+                                 win_len=win_len,
+                                 ov=overlap,
+                                 perm=bool_to_str(isPerm),
+                                 alg=alg,
+                                 adj=adj,
+                                 avgTm=bool_to_str(tm_avg),
+                                 avgTst=bool_to_str(tst_avg),
+                                 inst=ni,
+                                 rsP=rs,
+                                 mode='acc')
+
+        if not os.path.isfile(fname + '.npz'):
+            call(call_str, shell=True)
+        job_id += 1
+
+        while int(check_output(JOB_Q_CHECK, shell=True)) >= 200:
+            time.sleep(30)
