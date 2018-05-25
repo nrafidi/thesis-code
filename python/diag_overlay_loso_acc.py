8f0ec75a16d85d82b964ed7ba31ff393470a7022
Nicole <nicole.rafidi@gmail.com>
2018-05-25 09:50:25 -0400

    adjusting default

--- python/diag_overlay_loso_acc.py
+++ python/diag_overlay_loso_acc.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     parser.add_argument('--overlap', type=int, default=12)
     parser.add_argument('--alg', default='lr-l2', choices=['lr-l2', 'lr-l1'])
     parser.add_argument('--adj', default='zscore', choices=['None', 'mean_center', 'zscore'])
-    parser.add_argument('--num_instances', type=int, default=2)
+    parser.add_argument('--num_instances', type=int, default=10)
     parser.add_argument('--avgTime', default='F')
     parser.add_argument('--avgTest', default='T')
     parser.add_argument('--sig_test', default='binomial', choices=['binomial', 'wilcoxon'])
