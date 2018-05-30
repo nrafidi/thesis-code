8c3bbfa7dbe5151a9144dc01112aeb1ccbe46592
Nicole <nicole.rafidi@gmail.com>
2018-05-29 21:36:36 -0400

    plot adjustments

--- python/plot_data_avg.py
+++ python/plot_data_avg.py
@@ -73,17 +73,17 @@ if __name__ == '__main__':
                                                                                        is_region_sorted=False,
                                                                                        tmin=None,
                                                                                        tmax=None)
-        data_to_plot = np.squeeze(data[0, sorted_inds, ::2])
+        data_to_plot = np.squeeze(data[0, sorted_inds, ::5])
         print(data_to_plot.shape)
-        time = time[::2]
+        time = time[::5]
         num_time = time.size
         ax = inst_grid[i_inst]
         im = ax.imshow(data_to_plot, interpolation='nearest', vmin=-0.4e-11,
                       vmax=0.4e-11)
         ax.set_yticks(yticks_sens)
         ax.set_yticklabels(uni_reg)
-        ax.set_xticks(range(0, num_time, 125))
-        time_labels = time[::125] + 0.5
+        ax.set_xticks(range(0, num_time, 50))
+        time_labels = time[::50] + 0.5
         time_labels[np.abs(time_labels) < 1e-10] = 0.0
         ax.set_xticklabels(time_labels)
         ax.tick_params(labelsize=ticklabelsize)
