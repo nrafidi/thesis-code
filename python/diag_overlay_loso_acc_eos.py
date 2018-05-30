58947a35a1368fbf80d4dd28522f11e6c88cb5ab
nrafidi <nicole.rafidi@gmail.com>
2018-05-30 11:12:13 -0400

    debugging for pa3 plotting

--- python/diag_overlay_loso_acc_eos.py
+++ python/diag_overlay_loso_acc_eos.py
@@ -59,11 +59,11 @@ def bhy_multiple_comparisons_procedure(uncorrected_pvalues, alpha=0.05, assume_i
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiment')
-    # parser.add_argument('--win_len', type=int, default=100)
+    parser.add_argument('--win_len', type=int, default=50)
     parser.add_argument('--overlap', type=int, default=12)
     parser.add_argument('--alg', default='lr-l2', choices=['lr-l2', 'lr-l1'])
     parser.add_argument('--adj', default='zscore', choices=['None', 'mean_center', 'zscore'])
-    # parser.add_argument('--num_instances', type=int, default=2)
+    parser.add_argument('--num_instances', type=int, default=2)
     parser.add_argument('--avgTime', default='F')
     parser.add_argument('--avgTest', default='T')
     parser.add_argument('--sig_test', default='binomial', choices=['binomial', 'wilcoxon'])
@@ -79,9 +79,8 @@ if __name__ == '__main__':
     else:
         aTst = ''
 
-    PARAMS = {'pooled': {'win_len': 50, 'ni': 2},
-              'active': {'win_len': 50, 'ni': 2},
-              'passive': {'win_len': 50, 'ni': 2},}
+    win_len = args.win_len
+    num_instances = args.num_instances
 
     ticklabelsize = 14
     legendfontsize = 16
@@ -95,9 +94,7 @@ if __name__ == '__main__':
     sen_type_list = ['active', 'passive', 'pooled']
     sen_fig, sen_axs = plt.subplots(1, len(sen_type_list), figsize=(36, 12))
     for i_sen_type, sen_type in enumerate(sen_type_list):
-        win_len = PARAMS[sen_type]['win_len']
         time_adjust = win_len * 0.002
-        num_instances = PARAMS[sen_type]['ni']
         if args.experiment == 'krns2':
             word_list = ['verb', 'agent', 'patient']
             if num_instances > 1:
--- python/tgm_loso_acc_eos.py
+++ python/tgm_loso_acc_eos.py
@@ -56,10 +56,16 @@ CHANCE = {'krns2':{'pooled': {'noun1': 0.125,
                                   'senlen': 0.5},
                   'active': {'noun1': 0.25,
                              'verb': 0.25,
-                             'voice': 0.5},
+                             'voice': 0.5,
+                                  'agent': 0.25,
+                                  'patient': 0.25,
+                              'propid': 1.0/8.0,},
                   'passive': {'noun1': 0.25,
                              'verb': 0.25,
-                             'voice': 0.5}
+                             'voice': 0.5,
+                                  'agent': 0.25,
+                                  'patient': 0.25,
+                              'propid': 1.0/8.0,}
                     }}
 
 def intersect_accs(exp,
