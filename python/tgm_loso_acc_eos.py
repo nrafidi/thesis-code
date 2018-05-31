1c74e0c39fcd742b306f1d6a5e36c9fa97bb65ab
nrafidi <nicole.rafidi@gmail.com>
2018-05-31 11:09:43 -0400

    adjust tgm scales

--- python/tgm_loso_acc_eos.py
+++ python/tgm_loso_acc_eos.py
@@ -174,12 +174,12 @@ if __name__ == '__main__':
                   'propid': 0.2}
     else:
         word_list = word_list = ['voice', 'noun1', 'verb', 'agent', 'patient', 'propid']
-        vmaxes = {'noun1': 0.5,
-                  'agent': 0.5,
-                  'patient': 0.5,
-                  'verb': 0.5,
+        vmaxes = {'noun1': 0.35,
+                  'agent': 0.35,
+                  'patient': 0.35,
+                  'verb': 0.35,
                   'voice': 1.0,
-                  'propid': 0.25}
+                  'propid': 0.2}
     num_plots = len(word_list)/2
     time_step = int(250 / args.overlap)
     time_adjust = args.win_len * 0.002 * time_step
