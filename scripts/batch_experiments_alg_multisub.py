390a349e734bb9b7f4294151c3bff2d033efe46b
Nicole <nicole.rafidi@gmail.com>
2018-06-10 16:28:42 -0400

    rerun stuck jobs

--- scripts/batch_experiments_alg_multisub.py
+++ scripts/batch_experiments_alg_multisub.py
@@ -97,5 +97,5 @@ if __name__ == '__main__':
             call(call_str, shell=True)
         job_id += 1
 
-        while int(check_output(JOB_Q_CHECK, shell=True)) >= 200:
-            time.sleep(30)
+        # while int(check_output(JOB_Q_CHECK, shell=True)) >= 200:
+        #     time.sleep(30)
