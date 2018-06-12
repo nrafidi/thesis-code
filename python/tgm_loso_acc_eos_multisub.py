e4f02807a0d7e89df653da8abbab3f6fc7991001
Nicole <nicole.rafidi@gmail.com>
2018-06-12 14:30:55 -0400

    redo figures with rank accuracy

--- python/tgm_loso_acc_eos_multisub.py
+++ python/tgm_loso_acc_eos_multisub.py
@@ -10,6 +10,7 @@ from math import ceil
 import run_TGM_LOSO_EOS
 from mpl_toolkits.axes_grid1 import AxesGrid
 import string
+from rank_from_pred import rank_from_pred
 
 PLOT_TITLE_EXP = {'krns2': 'Pilot Experiment',
                   'PassAct3': 'Final Experiment'}
@@ -110,22 +111,9 @@ if __name__ == '__main__':
     if args.experiment == 'krns2':
         if sen_type == 'pooled':
             word_list.extend(['noun1', 'voice', 'propid'])
-        vmaxes = {'noun1': 0.3,
-                  'agent': 0.5,
-                  'patient': 0.5,
-                  'verb': 0.75,
-                  'voice': 1.0,
-                  'propid': 0.25}
     else:
         if sen_type == 'pooled':
             word_list.extend(['senlen', 'noun1', 'voice', 'propid'])
-        vmaxes = {'noun1': 0.5,
-                  'agent': 0.5,
-                  'patient': 0.5,
-                  'verb': 0.5,
-                  'voice': 1.0,
-                  'senlen': 1.0,
-                  'propid': 0.5}
     if sen_type == 'pooled':
         n_rows=2
     else:
@@ -135,11 +123,10 @@ if __name__ == '__main__':
     time_adjust = args.win_len * 0.002 * time_step
     combo_fig = plt.figure(figsize=(num_plots*6, 12))
     combo_grid = AxesGrid(combo_fig, 111, nrows_ncols=(n_rows, num_plots),
-                          axes_pad=0.7, cbar_mode='each', cbar_location='right',
+                          axes_pad=0.7, cbar_mode='single', cbar_location='right',
                           cbar_pad=0.2, share_all=True)
 
     for i_word, word in enumerate(word_list):
-        chance = CHANCE[args.experiment][sen_type][word]
         top_dir = TOP_DIR.format(exp=args.experiment)
         multi_file = MULTI_SAVE_FILE.format(dir=top_dir,
                                             sen_type=sen_type,
@@ -156,14 +143,22 @@ if __name__ == '__main__':
                                             mode='acc')
 
         result = np.load(multi_file + '.npz')
-        multi_fold_acc = result['tgm_acc']
+        tgm_pred = result['tgm_pred']
+        l_ints = result['l_ints']
+        cv_membership = result['cv_membership']
+        fold_labels = []
+        for i in range(len(cv_membership)):
+            fold_labels.append(np.mean(l_ints[cv_membership[i]]))
+
+        multi_fold_acc = rank_from_pred(tgm_pred, fold_labels)
         time = result['time']
         win_starts = result['win_starts']
 
         mean_acc = np.mean(multi_fold_acc, axis=0)
 
         ax = combo_grid[i_word]
-        im = ax.imshow(np.squeeze(mean_acc), interpolation='nearest', aspect='auto', vmin=chance, vmax=vmaxes[word])
+        im = ax.imshow(np.squeeze(mean_acc), interpolation='nearest', aspect='auto',
+                       vmin=0.5, vmax=1.0)
 
         ax.set_title('{word}'.format(
             word=PLOT_TITLE_WORD[word]), fontsize=axistitlesize)
@@ -188,7 +183,7 @@ if __name__ == '__main__':
     # for cax in combo_grid.cbar_axes:
     #     cax.toggle_label(False)
 
-    combo_fig.suptitle('TGM Averaged Over Subjects\n{}'.format(PLOT_TITLE_SEN[sen_type]),
+    combo_fig.suptitle('Rank Accuracy TGMs\n{}'.format(PLOT_TITLE_SEN[sen_type]),
                        fontsize=suptitlesize)
     combo_fig.text(0.04, 0.275, 'Train Time Relative to Last Word Onset (s)', va='center',
                    rotation=90, rotation_mode='anchor', fontsize=axislabelsize)
