c32203818659799423b79cb108a7a465849dbed4
Nicole <nicole.rafidi@gmail.com>
2018-06-12 14:56:01 -0400

    precompute rank accuracies for main tasks

--- python/rank_from_pred.py
+++ python/rank_from_pred.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import os.path
 
 def rank_from_pred(tgm_pred, fold_labels):
     rank_acc = np.empty(tgm_pred.shape)
@@ -21,23 +22,62 @@ def rank_from_pred(tgm_pred, fold_labels):
 
 
 if __name__ == '__main__':
-    result = np.load('/share/volume0/nrafidi/PassAct3_TGM_LOSO/TGM-LOSO_multisub_passive_noun2_win50_ov5_prF_' \
-            'alglr-l2_adj-zscore_avgTimeT_avgTestT_ni2_rsPerm1_acc.npz')
-    tgm_pred = result['tgm_pred']
-    l_ints = result['l_ints']
-    # print(l_ints)
-    cv_membership = result['cv_membership']
-    # print(len(cv_membership))
-    fold_labels = []
-    for i in range(len(cv_membership)):
-        fold_labels.append(np.mean(l_ints[cv_membership[i]]))
-    # print(cv_membership)
-    rank_acc = rank_from_pred(tgm_pred, fold_labels)
-    fig, ax = plt.subplots()
-    im = ax.imshow(np.mean(rank_acc, axis=0), interpolation='nearest', vmin=0.5, vmax=1.0)
-    plt.colorbar(im)
 
-    fig, ax = plt.subplots()
-    ax.plot(np.diag(np.mean(rank_acc, axis=0)))
-    ax.set_ylim([0.0, 1.0])
-    plt.show()
+    fname = '/share/volume0/nrafidi/{exp}_TGM_LOSO/TGM-LOSO_multisub_{sen_type}_{word}_win50_ov5_prF_' \
+            'alglr-l2_adj-zscore_avgTimeT_avgTestT_ni2_rsPerm1_{rank_str}acc.npz'
+    for exp in ['krns2', 'PassAct3']:
+        for sen_type in ['active', 'passive']:
+            for word in ['noun1', 'verb', 'noun2']:
+                fname_load = fname.format(exp=exp, sen_type=sen_type,
+                                          word=word, rank_str='')
+                fname_save = fname.format(exp=exp, sen_type=sen_type,
+                                          word=word, rank_str='rank')
+                if not os.path.isfile(fname_load):
+                    continue
+                if os.path.isfile(fname_save):
+                    continue
+                result = np.load(fname_load)
+                tgm_pred = result['tgm_pred']
+                l_ints = result['l_ints']
+                cv_membership = result['cv_membership']
+                fold_labels = []
+                for i in range(len(cv_membership)):
+                    fold_labels.append(np.mean(l_ints[cv_membership[i]]))
+                tgm_rank = rank_from_pred(tgm_pred, fold_labels)
+
+                np.savez_compressed(fname_save,
+                                    tgm_rank=tgm_rank)
+
+    fname = '/share/volume0/nrafidi/{exp}_TGM_LOSO_EOS/TGM-LOSO-EOS_multisub_{sen_type}_{word}_win50_ov5_prF_' \
+            'alglr-l2_adj-zscore_avgTimeT_avgTestT_ni2_rsPerm1_{rank_str}acc.npz'
+    for exp in ['krns2', 'PassAct3']:
+        for sen_type in ['active', 'passive', 'pooled']:
+            for word in ['noun1', 'verb', 'agent', 'patient', 'voice', 'propid', 'senlen']:
+                fname_load = fname.format(exp=exp, sen_type=sen_type,
+                                          word=word, rank_str='')
+                fname_save = fname.format(exp=exp, sen_type=sen_type,
+                                          word=word, rank_str='rank')
+                if not os.path.isfile(fname_load):
+                    continue
+                if os.path.isfile(fname_save):
+                    continue
+                result = np.load(fname_load)
+                tgm_pred = result['tgm_pred']
+                l_ints = result['l_ints']
+                cv_membership = result['cv_membership']
+                fold_labels = []
+                for i in range(len(cv_membership)):
+                    fold_labels.append(np.mean(l_ints[cv_membership[i]]))
+                tgm_rank = rank_from_pred(tgm_pred, fold_labels)
+
+                np.savez_compressed(fname_save,
+                                    tgm_rank=tgm_rank)
+
+    # fig, ax = plt.subplots()
+    # im = ax.imshow(np.mean(rank_acc, axis=0), interpolation='nearest', vmin=0.5, vmax=1.0)
+    # plt.colorbar(im)
+    #
+    # fig, ax = plt.subplots()
+    # ax.plot(np.diag(np.mean(rank_acc, axis=0)))
+    # ax.set_ylim([0.0, 1.0])
+    # plt.show()
