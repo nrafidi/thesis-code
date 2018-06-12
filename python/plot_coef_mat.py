2a9f15011f6891d5879400428bfe76d796186d80
Nicole <nicole.rafidi@gmail.com>
2018-06-12 12:39:06 -0400

    first attempt at helmet plots

--- python/plot_coef_mat.py
+++ python/plot_coef_mat.py
@@ -6,18 +6,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 import run_coef_TGM_multisub
+from mne.viz import plot_topomap
+from syntax_vs_semantics import sensor_metadata
+from mpl_toolkits.axes_grid1 import AxesGrid
 
 
-SENSOR_MAP = '/bigbrain/bigbrain.usr1/homes/nrafidi/MATLAB/groupRepo/shared/megVis/sensormap.mat'
+# SENSOR_MAP = '/bigbrain/bigbrain.usr1/homes/nrafidi/MATLAB/groupRepo/shared/megVis/sensormap.mat'
+
+#
+# def sort_sensors():
+#     load_var = sio.loadmat(SENSOR_MAP)
+#     sensor_reg = load_var['sensor_reg']
+#     sensor_reg = [str(sens[0][0]) for sens in sensor_reg]
+#     sorted_inds = np.argsort(sensor_reg)
+#     sorted_reg = [sensor_reg[ind] for ind in sorted_inds]
+#     return sorted_inds, sorted_reg
 
 
-def sort_sensors():
-    load_var = sio.loadmat(SENSOR_MAP)
-    sensor_reg = load_var['sensor_reg']
-    sensor_reg = [str(sens[0][0]) for sens in sensor_reg]
-    sorted_inds = np.argsort(sensor_reg)
-    sorted_reg = [sensor_reg[ind] for ind in sorted_inds]
-    return sorted_inds, sorted_reg
+def _make_coordinates(sensor_layout, sensor_type_id):
+    coordinates = np.hstack(
+        [np.expand_dims(np.array([si.x_coord for si in sensor_layout if si.sensor_type_id == sensor_type_id]), 1),
+         np.expand_dims(np.array([si.y_coord for si in sensor_layout if si.sensor_type_id == sensor_type_id]), 1)])
+    # coordinates is now sensors by xy
+    return coordinates
 
 
 if __name__ == '__main__':
@@ -31,6 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--adj', default='zscore', choices=['None', 'mean_center', 'zscore'])
     parser.add_argument('--num_instances', type=int, default=2)
     parser.add_argument('--avgTime', default='T')
+    parser.add_argument('--sensor_type_id', type=int, default=2)
     args = parser.parse_args()
 
     experiment = args.experiment
@@ -42,10 +54,15 @@ if __name__ == '__main__':
     adj = args.adj
     avgTime = args.avgTime
     num_instances = args.num_instances
+    sensor_id = args.sensor_type_id
 
-    sorted_inds, sorted_reg = sort_sensors()
-    uni_reg = np.unique(sorted_reg)
-    yticks_sens = [sorted_reg.index(reg) for reg in uni_reg]
+    tmin_to_plot = 0.0
+    tmax_to_plot = 0.3
+    t_step = overlap*0.002
+
+    sensor_layout = sensor_metadata.read_sensor_metadata()
+    coordinates = _make_coordinates(sensor_layout, sensor_id)
+    sensors_to_keep = [i_s for i_s, s in enumerate(sensor_layout) if s.sensor_type_id == sensor_id]
 
     top_dir = run_coef_TGM_multisub.TOP_DIR.format(exp=experiment)
 
@@ -64,50 +81,34 @@ if __name__ == '__main__':
     win_starts = result['win_starts']
     time = result['time'][win_starts]
 
-    maps_over_time = []
-    for tmin in np.arange(0.0, 0.3, 0.01):
 
-        time_to_plot = np.where(np.logical_and(time >= tmin, time <= 0.3))
+    time_to_plot = np.arange(tmin_to_plot, tmax_to_plot, t_step).tolist()
+    num_plots = len(time_to_plot)/2
+    combo_fig = plt.figure(figsize=(num_plots * 6, 12))
+    combo_grid = AxesGrid(combo_fig, 111, nrows_ncols=(2, num_plots),
+                          axes_pad=0.7, cbar_mode='single', cbar_location='right',
+                          cbar_pad=0.2)
+    for i_t, tmin in enumerate(time_to_plot):
+        time_to_plot = np.where(np.logical_and(time >= tmin, time <= tmin + t_step))
         time_to_plot = time_to_plot[0][0]
         print(time_to_plot)
         map = maps[time_to_plot]
-
-        # fig, ax = plt.subplots()
-        # im = ax.imshow(map, interpolation='nearest', aspect='auto')
-        # fig.suptitle('Raw importance map')
-        # fig.colorbar(im)
-
         class_map = np.reshape(np.mean(map, axis=0), (1, -1))
-        # fig, ax = plt.subplots()
-        # im = ax.imshow(class_map, interpolation='nearest', aspect='auto')
-        # fig.suptitle('Map averaged over classes')
-        # fig.colorbar(im)
-
         sub_map = np.zeros((1, 306))
         for i_sub in range(len(run_coef_TGM_multisub.VALID_SUBS[experiment])):
             start_ind = i_sub*306
             end_ind = start_ind + 306
             sub_map += class_map[:, start_ind:end_ind]
         sub_map /= len(run_coef_TGM_multisub.VALID_SUBS[experiment])
+        sub_map = sub_map[sensors_to_keep]
+        print(np.min(sub_map))
+        print(np.max(sub_map))
+        ax = combo_grid[i_t]
+        im, _ = plot_topomap(sub_map, coordinates, axes=ax, show=False)
+        ax.set_title('%.2f-%.2f' % tmin, tmin + win_len*0.002)
 
+    cbar = combo_grid.cbar_axes[i_t].colorbar(im)
 
-        maps_over_time.append(sub_map)
-        # sub_map = sub_map[:, sorted_inds]
-        # fig, ax = plt.subplots(figsize=(12, 12))
-        # h = ax.imshow(sub_map, interpolation='nearest', aspect='auto')
-        # ax.set_xticks(yticks_sens)
-        # ax.set_xticklabels(uni_reg)
-        # ax.set_xlabel('Sensors')
-        # fig.suptitle('Map averaged over classes and Subjects')
-        # fig.colorbar(h)
-
-    maps_over_time = np.concatenate(maps_over_time, axis=0)
-
-    sio.savemat('maps_over_time_{exp}_{sen}_{word}.mat'.format(exp=experiment,
-                                                               sen=sen_type,
-                                                               word=word),
-                mdict={'maps_over_time': maps_over_time})
-
-    # plt.show()
+    plt.show()
 
 
