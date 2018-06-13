fd97fae54f878170a39c20a099230b65ef0ea5b6
Nicole <nicole.rafidi@gmail.com>
2018-06-12 20:03:56 -0400

    check which jobs have failed

--- scripts/check_jobs_eos_multisub_coef.py
+++ scripts/check_jobs_eos_multisub_coef.py
@@ -67,9 +67,10 @@ if __name__ == '__main__':
             was_success = False
 
         if not was_success:
+            print(job_id)
             if not os.path.isfile(err_str) or not os.path.isfile(out_str):
                 # print('Job {} Did Not Run'.format(job_str))
-                print(job_id)
+                # print(job_id)
                 meow = 1
             else:
                 with open(out_str, 'r') as fid:
@@ -80,9 +81,9 @@ if __name__ == '__main__':
                 elif os.stat(err_str).st_size != 0:
                     with open(err_str, 'r') as fid:
                         err_file = fid.read()
-                        print(err_file)
+                        # print(err_file)
                         print(grid)
-                        print(job_id)
+                        # print(job_id)
                         # if not err_file.endswith('warnings.warn(_use_error_msg)\n') and not ('Killed' in err_file):
                         #     if 'MemoryError' in err_file:
                         #         print('Job {} Failed Memory Error'.format(job_str))
