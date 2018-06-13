3eb1d0a77e791d0f70c1379e333636a9350cbd44
Nicole <nicole.rafidi@gmail.com>
2018-06-12 20:01:43 -0400

    check which jobs have failed

--- scripts/check_jobs_eos_multisub_coef.py
+++ scripts/check_jobs_eos_multisub_coef.py
@@ -81,6 +81,7 @@ if __name__ == '__main__':
                         err_file = fid.read()
                         print(err_file)
                         print(grid)
+                        print(job_id)
                         # if not err_file.endswith('warnings.warn(_use_error_msg)\n') and not ('Killed' in err_file):
                         #     if 'MemoryError' in err_file:
                         #         print('Job {} Failed Memory Error'.format(job_str))
