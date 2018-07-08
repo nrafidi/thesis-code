dee24b8dfd1c9fd9cf75cc4ae7aeecb68f9444d4
Nicole <nicole.rafidi@gmail.com>
2018-07-08 18:16:46 -0400

    plot adjustments

--- python/diag_overlay_loso_acc_eos.py
+++ python/diag_overlay_loso_acc_eos.py
@@ -213,11 +213,11 @@ if __name__ == '__main__':
             ax.set_ylabel('Accuracy', fontsize=axislabelsize)
         if i_sen_type == 1:
             ax.set_xlabel('Time Relative to Last Word Onset (s)', fontsize=axislabelsize)
-        ax.set_ylim([0.0, 1.0])
+            ax.set_ylim([0.0, 1.2])
         ax.set_xlim([0, len(time[win_starts]) + 0.8*time_step])
         ax.tick_params(labelsize=ticklabelsize)
         if sen_type == 'pooled':
-            ax.legend(bbox_to_anchor=(0.6, 0.95), loc=2, borderaxespad=0., ncol=1, fontsize=legendfontsize)
+            ax.legend(loc=3, ncol=2, fontsize=legendfontsize)
         ax.set_title('{sen_type}'.format(sen_type=PLOT_TITLE_SEN[sen_type]), fontsize=axistitlesize)
         ax.text(-0.05, 1.05, string.ascii_uppercase[i_sen_type], transform=ax.transAxes,
                 size=axislettersize, weight='bold')
