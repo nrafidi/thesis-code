dd7a6914c50ac11432b964c5312a0efb2b0982b2
Nicole <nicole.rafidi@gmail.com>
2018-07-23 14:57:38 -0400

    testing colormap

--- python/source_hp_loso_acc_eos_multisub.py
+++ python/source_hp_loso_acc_eos_multisub.py
@@ -183,12 +183,12 @@ if __name__ == '__main__':
     cmap = 'jet'
 
     lims = [0.0, 0.5, 1.0]  # based on min and max avrg correlation values over all models
-    smoothing_steps = 1
+    smoothing_steps = 10
     bk = 'white'
     fname = "/home/nrafidi/thesis_figs/krns2_pooled_{}_eos-{}".format(word, args.plot_type)
 
     f0 = mne.viz.plot_source_estimates(src, subject=STRUCTURAL, background=bk, surface='inflated', hemi='lh', views='lat',
-                                      subjects_dir=SUBJ_DIR,
+                                      clim='auto', colormap=cmap, subjects_dir=SUBJ_DIR,
                                       smoothing_steps=smoothing_steps, spacing='ico4', backend='matplotlib')
 
     f0.savefig(fname + "_lh_med_source_plot.pdf", bbox_inches='tight')
