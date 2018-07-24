e122f090fef23bd1b6edf587afffd71d8f348d6e
Nicole <nicole.rafidi@gmail.com>
2018-07-23 22:21:05 -0400

    plot adjustments

--- python/source_hp_loso_acc_eos_multisub.py
+++ python/source_hp_loso_acc_eos_multisub.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
             # each vertex will show the average correlation for the corresponding region
             maxes_to_add = np.ones(len(label.vertices)) * max_over_time_left[region_index_in_results]
             if label_name == 'frontalpole':
-                maxes_to_add[0:70] = 1.0
+                maxes_to_add[0:100] = 1.0
             left_maxes_per_label = np.append(left_maxes_per_label,
                                                     maxes_to_add)
     # the vertices are required to be sorted for the plotting function
