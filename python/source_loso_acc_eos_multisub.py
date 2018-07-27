447cbe80c6a439cdcd5d8c1ee31aefac169a59ab
Nicole <nicole.rafidi@gmail.com>
2018-07-27 11:56:48 -0400

    plot adjustments

--- python/source_loso_acc_eos_multisub.py
+++ python/source_loso_acc_eos_multisub.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
 
     combo_fig = plt.figure(figsize=(12*len(HEMIS), 8))
     combo_grid = AxesGrid(combo_fig, 111, nrows_ncols=(1, len(HEMIS)),
-                          axes_pad=0.7, share_all=True, aspect=False)
+                          axes_pad=0.8, share_all=True, aspect=False)
     # cbar_mode='single', cbar_location='right',
     #                       cbar_pad=0.5,
     for i_hemi, hemi in enumerate(HEMIS):
